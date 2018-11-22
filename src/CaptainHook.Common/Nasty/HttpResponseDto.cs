d6e12d31890ae58a043edb1960b6d82baf993636
David Guerin <david.guerin@developershelf.com>
2018-11-22 17:13:58 +0000

    Wiring up the handler for

---
+++ src/CaptainHook.Common/Nasty/HttpResponseDto.cs
@@ -0,0 +1,15 @@
+﻿namespace CaptainHook.Common.Nasty
+{
+    /// <summary>
+    /// Really temp dto
+    /// todo nuke when building this out correctly
+    /// </summary>
+    public class HttpResponseDto
+    {
+        public int StatusCode { get; set; }
+
+        public string ReasonPhase { get; set; }
+
+        public string Content { get; set; }
+    }
+}
