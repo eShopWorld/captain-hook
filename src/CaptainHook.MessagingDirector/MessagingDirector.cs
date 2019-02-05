4d84824456717d48e7da2e15dd5833977faf7d99
David Guerin <david.guerin@developershelf.com>
2019-02-05 14:10:20 +0000

    Uncomment a comment

--- src/CaptainHook.MessagingDirector/MessagingDirector.cs
+++ src/CaptainHook.MessagingDirector/MessagingDirector.cs
@@ -35,8 +35,8 @@
             await StateManager.TryAddStateAsync(MessageTypesKey,
                 new[]
                 {
-                    //"checkout.domain.infrastructure.domainevents.retailerorderconfirmationdomainevent",
-                    //"checkout.domain.infrastructure.domainevents.platformordercreatedomainevent",
+                    "checkout.domain.infrastructure.domainevents.retailerorderconfirmationdomainevent",
+                    "checkout.domain.infrastructure.domainevents.platformordercreatedomainevent",
                     "nike.snkrs.core.events.productrefreshevent",
                     "nike.snkrs.core.events.productupdatedevent"
                 });
