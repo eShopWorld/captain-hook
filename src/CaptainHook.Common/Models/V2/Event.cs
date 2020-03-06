f6318ab0e2ffe54353c9201e175cdff8eca6d028
VaclavK <vaclav@vaclavkabat.net>
2020-03-06 12:04:53 +0000

    add event model definitions

---
+++ src/CaptainHook.Common/Models/V2/Event.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace CaptainHook.Common.Models.V2
+{
+    /// <summary>
+    /// Event definition model/POCO
+    /// </summary>
+    public class Event
+    {
+        /// <summary>
+        /// name of the event
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// underlying event (or message) full type name
+        /// </summary>
+        public string Type { get; set; }
+        /// <summary>
+        /// scope of visibility for the event instance
+        /// </summary>
+        public EventVisibilityEnum Visibility { get; set; } = EventVisibilityEnum.Internal;
+        /// <summary>
+        /// list of tenant codes to constraint the event visibility (if applicable)
+        /// </summary>
+        public IList<string> TenantCodes { get; set; }
+    }
+}
---
+++ src/CaptainHook.Common/Models/V2/EventVisibilityEnum.cs
@@ -0,0 +1,11 @@
+﻿namespace CaptainHook.Common.Models.V2
+{
+    /// <summary>
+    /// defines types of visibility of <see cref="Event"/>
+    /// </summary>
+    public enum EventVisibilityEnum
+    {
+        Internal,
+        Public
+    }
+}
