1b3e949c565b62dbd8a0954bb76e7cc0e784a510
VaclavK <vaclav@vaclavkabat.net>
2020-03-13 18:35:01 +0000

    remove unused usings and add inline doco

--- src/CaptainHook.Common/Telemetry/Message/UnroutableMessageEvent.cs
+++ src/CaptainHook.Common/Telemetry/Message/UnroutableMessageEvent.cs
@@ -1,14 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace CaptainHook.Common.Telemetry.Message
+﻿namespace CaptainHook.Common.Telemetry.Message
 {
+    /// <summary>
+    /// Custom event emitted when message seen that cannot be routed
+    /// </summary>
     public class UnroutableMessageEvent
     {
+        /// <summary>
+        /// type of the event
+        /// </summary>
         public string EventType { get; set; }
+        /// <summary>
+        /// value of the selector
+        /// </summary>
         public string Selector { get; set; }
+        /// <summary>
+        /// Name of the subscriber
+        /// </summary>
         public string SubscriberName { get; set; }
+        /// <summary>
+        /// Reason of failure in human readable form
+        /// </summary>
         public string Message { get; set; }
     }
 }
