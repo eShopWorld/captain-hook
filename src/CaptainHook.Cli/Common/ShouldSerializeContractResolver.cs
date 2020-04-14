abfd7c80ad3f4ac1439ff9576b0347074cfa4894
Antonino Parisi <aparisi@eshopworld.com>
2020-04-14 14:09:11 +0100

    Skip serialization of HttpVerb default value

--- src/CaptainHook.Cli/Common/ShouldSerializeContractResolver.cs
+++ src/CaptainHook.Cli/Common/ShouldSerializeContractResolver.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Reflection;
 
 namespace CaptainHook.Cli.Common
@@ -18,29 +19,34 @@ namespace CaptainHook.Cli.Common
         {
             JsonProperty property = base.CreateProperty(member, memberSerialization);
 
-            if (property.PropertyType != typeof(string))
+            // don't serialize empty collections
+            if (property.PropertyType != typeof(string) && property.PropertyType.GetInterface(nameof(IEnumerable)) != null)
             {
-                // don't serialize empty collections
-                if (property.PropertyType.GetInterface(nameof(IEnumerable)) != null)
-                {
-                    property.ShouldSerialize =
-                        instance => (instance?.GetType().GetProperty(property.PropertyName).GetValue(instance) as IEnumerable<object>)?.Count() > 0;
-                }
-
-                // don't serialize default timeout value of 100 seconds
-                if (property.PropertyType == typeof(TimeSpan) && property.PropertyName.Equals("Timeout", StringComparison.InvariantCulture))
-                {
-                    property.ShouldSerialize =
-                        instance => ((TimeSpan)instance.GetType().GetProperty(property.PropertyName).GetValue(instance)).TotalSeconds != 100;
-                }
-
-                // don't serialize empty AuthenticationConfig objects
-                if(property.PropertyType == typeof(AuthenticationConfig) && property.PropertyName.Equals("AuthenticationConfig", StringComparison.InvariantCulture))
-                {
-                    property.ShouldSerialize =
-                        instance => ((AuthenticationConfig)instance.GetType().GetProperty(property.PropertyName).GetValue(instance)).Type != AuthenticationType.None;
-                }
+                property.ShouldSerialize =
+                    instance => (instance?.GetType().GetProperty(property.PropertyName).GetValue(instance) as IEnumerable<object>)?.Count() > 0;
             }
+
+            // don't serialize default timeout value of 100 seconds
+            if (property.PropertyType == typeof(TimeSpan) && property.PropertyName.Equals("Timeout", StringComparison.InvariantCulture))
+            {
+                property.ShouldSerialize =
+                    instance => ((TimeSpan)instance.GetType().GetProperty(property.PropertyName).GetValue(instance)).TotalSeconds != 100;
+            }
+
+            // don't serialize empty AuthenticationConfig objects
+            if(property.PropertyType == typeof(AuthenticationConfig) && property.PropertyName.Equals("AuthenticationConfig", StringComparison.InvariantCulture))
+            {
+                property.ShouldSerialize =
+                    instance => ((AuthenticationConfig)instance.GetType().GetProperty(property.PropertyName).GetValue(instance)).Type != AuthenticationType.None;
+            }
+
+            // don't serialize default HttpVerb of Post
+            if (property.PropertyName.Equals("HttpVerb", StringComparison.InvariantCulture))
+            {
+                property.ShouldSerialize =
+                    instance => !((string)instance.GetType().GetProperty(property.PropertyName).GetValue(instance)).Equals(HttpMethod.Post.ToString(), StringComparison.InvariantCultureIgnoreCase);
+            }
+
             return property;
         }
     }
