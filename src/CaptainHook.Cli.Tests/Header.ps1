7de9c5f86650b78f8b9966129288ee75d2fd4503
Dawid Kaliszewski <dawid.kaliszewski@zartis.local>
2020-05-11 11:47:06 +0200

    work in progress

---
+++ src/CaptainHook.Cli.Tests/Header.ps1
@@ -0,0 +1,43 @@
+﻿function setConfig ([string]$Name, [string]$Value, [string]$VaultName) {
+
+    # $secret = Get-AzureKeyVaultSecret -VaultName $VaultName -Name $Name;
+    # if($secret -eq $Value){
+        # Write-Host 'Nothing to update $Name are the same'
+        # return
+    # }
+
+    Write-Host "Updating Key $Name"
+    $secretvalue = ConvertTo-SecureString $Value -AsPlainText -Force
+    $secret = Set-AzureKeyVaultSecret -VaultName $VaultName -Name $Name -SecretValue $secretvalue    
+    Write-Host "$Name has been updated"
+}
+
+function removeConfig ([string]$Name, [string]$Value, [string]$VaultName) {
+
+    $secret = Get-AzureKeyVaultSecret -VaultName $VaultName -Name $Name;
+    if($secret -eq $Value){
+        Write-Host 'Nothing to update $Name are the same'
+        return
+    }
+
+    Write-Host "Deleting Key $Name"
+    Remove-AzureKeyVaultSecret -VaultName $VaultName -Name $Name -Force -Confirm:$False
+    Write-Host "$Name has been deleted"
+}
+
+$KeyVault = 'esw-sample-kv-we'
+
+###Add
+setConfig 'AzureSubscriptionId' 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx' $KeyVault
+setConfig 'ServiceBusNamespace' 'aaa-bbb-cc' $KeyVault
+setConfig 'ServiceBusConnectionString' 'Endpoint=sb://aaa-bbb-cc.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=abcdefghijklmnopqrstuvwxyz' $KeyVault
+
+setConfig 'InstrumentationKey' 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx' $KeyVault
+setConfig 'CaptainHook--ServiceBusSubscriptionId' 'aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee' $KeyVault
+setConfig 'CaptainHook--ServiceBusConnectionString' 'Endpoint=sb://aaa-bbb-cc.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=abcdefghijklmnopqrstuvwxyz' $KeyVault 
+setConfig 'CaptainHook--ServiceBusNamespace' 'aaa-bbb-cc' $KeyVault
+
+setConfig 'CaptainHook--ApiName' 'Captain Hook' $KeyVault
+setConfig 'CaptainHook--ApiSecret' 'verylongandsecuresecret' $KeyVault
+setConfig 'CaptainHook--Authority' 'https://security-sts.d1.companyname.com' $KeyVault
+setConfig 'CaptainHook--RequiredScopes--1' 'aaa.bbb.ccc' $KeyVault
---
+++ src/CaptainHook.Cli.Tests/Integration/FullTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Eshopworld.Tests.Core;
+using FluentAssertions;
+using Xunit;
+
+namespace CaptainHook.Cli.Tests.Integration
+{
+    public class FullTests
+    {
+        [Fact, IsLayer1]
+        public void OutputFileShouldBeSameAsInputFile()
+        {
+            var toolDirectory = Directory.GetCurrentDirectory();
+
+            const string toolName = "CaptainHook.Cli.exe";
+            const string sourceFile = "SampleFileWithSubscribers.ps1";
+            const string jsonDirectory = "json";
+            const string resultFile = "Result.ps1";
+
+            string generateJsonCommand = $@"generate-json --input ""{sourceFile}"" --output ""{jsonDirectory}""";
+
+            var generateJsonProcess = Process.Start(toolName, generateJsonCommand);
+            generateJsonProcess.WaitForExit();
+
+            File.Copy("Header.ps1", $@"{jsonDirectory}\Header.ps1", true);
+            
+            string generatePowerShellCommand = $@"generate-powershell --input ""{jsonDirectory}"" --output ""{resultFile}""";
+
+            var generatePowerShellProcess = Process.Start(toolName, generatePowerShellCommand);
+            generatePowerShellProcess.WaitForExit();
+
+            generateJsonProcess.ExitCode.Should().Be(0);
+            generatePowerShellProcess.ExitCode.Should().Be(0);
+
+            var original = CleanPowershell(File.ReadAllLines(sourceFile)).ToArray();
+            var result = CleanPowershell(File.ReadAllLines(resultFile)).ToArray();
+
+            result.Should().BeEquivalentTo(original);
+
+            /*generate-json --input "C:\Input\KeyVaultConfigV2_CI.ps1" --output "C:\Input\Output"
+generate-json --input "C:\Input\SampleFile.ps1" --output "C:\Input\jsonsample"*/
+        }
+
+        private IEnumerable<string> CleanPowershell(IEnumerable<string> input)
+        {
+            return input.Where(s => s.TrimStart().StartsWith("set")).OrderBy(s => s);
+        }
+    }
+}
