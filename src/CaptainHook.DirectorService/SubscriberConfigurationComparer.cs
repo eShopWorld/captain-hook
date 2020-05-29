67f47dbb8b2ee6af2f628582202084a7f8a4b8e2
dawidkeshop <dkaliszewski@eshopworld.com>
2020-05-29 13:27:15 +0200

    Code cleanup

---
+++ src/CaptainHook.DirectorService/SubscriberConfigurationComparer.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CaptainHook.Common.Configuration;
+using Newtonsoft.Json;
+
+namespace CaptainHook.DirectorService
+{
+    public class SubscriberConfigurationComparer
+    {
+        public Result Compare(IDictionary<string, SubscriberConfiguration> oldConfig, IDictionary<string, SubscriberConfiguration> newConfig)
+        {
+            var added = new Dictionary<string, SubscriberConfiguration>(newConfig.Where(x => !oldConfig.Keys.Contains(x.Key)));
+            var removed = new Dictionary<string, SubscriberConfiguration>(oldConfig.Where(x => !newConfig.Keys.Contains(x.Key)));
+
+            var changed = new Dictionary<string, SubscriberConfiguration>();
+            var keysToCheck = oldConfig.Keys.Intersect(newConfig.Keys);
+            foreach (var key in keysToCheck)
+            {
+                var previous = JsonConvert.SerializeObject(oldConfig[key]);
+                var current = JsonConvert.SerializeObject(newConfig[key]);
+
+                if (previous != current)
+                {
+                    changed.Add(key, newConfig[key]);
+                }
+            }
+
+            return new Result(added, removed, changed);
+        }
+
+        public class Result
+        {
+            public IDictionary<string, SubscriberConfiguration> Added { get; }
+            public IDictionary<string, SubscriberConfiguration> Removed { get; }
+            public IDictionary<string, SubscriberConfiguration> Changed { get; }
+            public bool HasChanged => this.Added.Any() || this.Removed.Any() || this.Changed.Any();
+
+            public Result(IDictionary<string, SubscriberConfiguration> added, IDictionary<string, SubscriberConfiguration> removed, IDictionary<string, SubscriberConfiguration> changed)
+            {
+                Added = added;
+                Removed = removed;
+                Changed = changed;
+            }
+        }
+    }
+}
---
+++ src/Tests/CaptainHook.Tests/Builders/WebhookRequestRuleBuilder.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using CaptainHook.Common.Authentication;
+using CaptainHook.Common.Configuration;
+
+namespace CaptainHook.Tests.Builders
+{
+    internal class WebhookRequestRuleBuilder
+    {
+        private ParserLocation source;
+        private ParserLocation destination;
+        private List<WebhookConfigRoute> routes;
+
+        public WebhookRequestRuleBuilder WithSource(string path = null, DataType type = DataType.Property, Location location = Location.Body)
+        {
+            this.source = new ParserLocation
+            {
+                Path = path,
+                Location = location,
+                Type = type,
+            };
+
+            return this;
+        }
+
+        public WebhookRequestRuleBuilder WithDestination(string path = null, DataType type = DataType.Property, Location location = Location.Body)
+        {
+            this.destination = new ParserLocation
+            {
+                Path = path,
+                Location = location,
+                Type = type,
+            };
+
+            return this;
+        }
+
+        public WebhookRequestRuleBuilder AddRoute(string selector, string uri)
+        {
+            if (this.routes == null)
+            {
+                this.routes = new List<WebhookConfigRoute>();
+            }
+
+            var route = new WebhookConfigRoute
+            {
+                Selector = selector,
+                Uri = uri,
+                AuthenticationConfig = new BasicAuthenticationConfig(),
+                HttpVerb = "POST"
+            };
+            this.routes.Add(route);
+
+            return this;
+        }
+
+        public WebhookRequestRule Create()
+        {
+            var rule = new WebhookRequestRule
+            {
+                Source = this.source,
+                Destination = this.destination,
+                Routes = this.routes
+            };
+
+            return rule;
+        }
+    }
+}
