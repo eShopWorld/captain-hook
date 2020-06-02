ae1add3958ea0637043f4a8cfbc4c6810351cf73
dawidkeshop <63288056+dawidkeshop@users.noreply.github.com>
2020-06-02 14:30:07 +0200

    69911 subscriber configuration comparison (#84)

---
+++ src/CaptainHook.DirectorService/SubscriberConfigurationComparer.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using CaptainHook.Common.Configuration;
+using Newtonsoft.Json;
+
+namespace CaptainHook.DirectorService
+{
+    public class SubscriberConfigurationComparer
+    {
+        /// <summary>
+        /// Compares previous and current subscriber configurations.
+        /// </summary>
+        /// <param name="oldConfig">Old configuration - the one which is currently deployed.</param>
+        /// <param name="newConfig">New configuration - the one which is up to date, but hasn't been deployed yet.</param>
+        /// <returns>A comparision result which contains three collections of subscriber configurations:
+        /// 1. Added - new subscribers which don't exist in old configuration list but exist in new.
+        /// 2. Removed - subscribers whose exist in old configurations list but are absent in new configuration.
+        /// 3. Changes - subscribers whose exist in both configurations, but some of configuration parameters has been changed.
+        /// </returns>
+        public Result Compare(IDictionary<string, SubscriberConfiguration> oldConfig, IDictionary<string, SubscriberConfiguration> newConfig)
+        {
+            var added = new Dictionary<string, SubscriberConfiguration>(newConfig.Where(x => !oldConfig.Keys.Contains(x.Key)));
+            var removed = new Dictionary<string, SubscriberConfiguration>(oldConfig.Where(x => !newConfig.Keys.Contains(x.Key)));
+
+            var changed = new Dictionary<string, SubscriberConfiguration>();
+            var keysToCheck = oldConfig.Keys.Intersect(newConfig.Keys);
+            foreach (var key in keysToCheck)
+            {
+                var previous = JsonConvert.SerializeObject(oldConfig[key]);
+                var current = JsonConvert.SerializeObject(newConfig[key]);
+
+                if (previous != current)
+                {
+                    changed.Add(key, newConfig[key]);
+                }
+            }
+
+            return new Result(added, removed, changed);
+        }
+
+        public class Result
+        {
+            public IReadOnlyDictionary<string, SubscriberConfiguration> Added { get; }
+            public IReadOnlyDictionary<string, SubscriberConfiguration> Removed { get; }
+            public IReadOnlyDictionary<string, SubscriberConfiguration> Changed { get; }
+            public bool HasChanged => this.Added.Any() || this.Removed.Any() || this.Changed.Any();
+
+            public Result(IDictionary<string, SubscriberConfiguration> added, IDictionary<string, SubscriberConfiguration> removed, IDictionary<string, SubscriberConfiguration> changed)
+            {
+                Added = new ReadOnlyDictionary<string, SubscriberConfiguration>(added);
+                Removed = new ReadOnlyDictionary<string, SubscriberConfiguration>(removed);
+                Changed = new ReadOnlyDictionary<string, SubscriberConfiguration>(changed);
+            }
+        }
+    }
+}
---
+++ src/Tests/CaptainHook.Tests/Builders/SubscriberConfigurationBuilder.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using CaptainHook.Common.Authentication;
+using CaptainHook.Common.Configuration;
+
+namespace CaptainHook.Tests.Builders
+{
+    internal class SubscriberConfigurationBuilder
+    {
+        private string type = "event1";
+        private string subscriberName = "captain-hook";
+        private string uri = "https://blah.blah.eshopworld.com";
+        private HttpMethod httpMethod = HttpMethod.Post;
+        private AuthenticationConfig authenticationConfig = new BasicAuthenticationConfig
+        {
+            Type = AuthenticationType.Basic,
+            Username = "user",
+            Password = "password",
+        };
+        private List<WebhookRequestRule> webhookRequestRules;
+        private WebhookConfig callback;
+
+        public SubscriberConfigurationBuilder WithType(string type)
+        {
+            this.type = type;
+            return this;
+        }
+
+        public SubscriberConfigurationBuilder WithSubscriberName(string subscriberName)
+        {
+            this.subscriberName = subscriberName;
+            return this;
+        }
+
+        public SubscriberConfigurationBuilder WithUri(string uri)
+        {
+            this.uri = uri;
+            return this;
+        }
+
+        public SubscriberConfigurationBuilder WithOidcAuthentication()
+        {
+            this.authenticationConfig = new OidcAuthenticationConfig
+            {
+                Type = AuthenticationType.OIDC,
+                Uri = "https://blah-blah.sts.eshopworld.com",
+                ClientId = "ClientId",
+                ClientSecret = "ClientSecret",
+                Scopes = new[] { "scope1", "scope2" }
+            };
+
+            return this;
+        }
+
+        public SubscriberConfigurationBuilder WithCallback(string uri)
+        {
+            this.callback = new WebhookConfig
+            {
+                Name = "callback",
+                HttpMethod = HttpMethod.Post,
+                Uri = uri,
+                EventType = "event1",
+                AuthenticationConfig = new AuthenticationConfig
+                {
+                    Type = AuthenticationType.None
+                },
+            };
+
+            return this;
+        }
+
+        public SubscriberConfigurationBuilder AddWebhookRequestRule(Action<WebhookRequestRuleBuilder> ruleBuilder)
+        {
+            if (this.webhookRequestRules == null)
+            {
+                this.webhookRequestRules = new List<WebhookRequestRule>();
+            }
+
+            var rb = new WebhookRequestRuleBuilder();
+            ruleBuilder(rb);
+            this.webhookRequestRules.Add(rb.Create());
+            return this;
+        }
+
+        public SubscriberConfiguration Create()
+        {
+            var subscriber = new SubscriberConfiguration
+            {
+                Name = this.type,
+                EventType = this.type,
+                SubscriberName = this.subscriberName,
+                HttpMethod = this.httpMethod,
+                Uri = this.uri,
+                AuthenticationConfig = this.authenticationConfig,
+                Callback = this.callback,
+                WebhookRequestRules = this.webhookRequestRules,
+            };
+
+            return subscriber;
+        }
+    }
+}
