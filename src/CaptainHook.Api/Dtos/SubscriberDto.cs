1f7351a2d363d9de91603c1ea539e7149c392b2e
dawidkeshop <dkaliszewski@eshopworld.com>
2020-06-19 13:00:10 +0200

    DTOs defined

---
+++ src/CaptainHook.Api/Dtos/SubscriberDto.cs
@@ -0,0 +1,11 @@
+﻿namespace CaptainHook.Api.Dtos
+{
+    public class SubscriberDto
+    {
+        public string Name { get; set; }
+
+        public string EventName { get; set; }
+
+        public WebhooksDto Webhooks { get; set; }
+    }
+}
---
+++ src/CaptainHook.Api/Dtos/WebhooksDto.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace CaptainHook.Api.Dtos
+{
+    public class WebhooksDto
+    {
+        public string SelectionRule { get; set; }
+
+        public List<EndpointDto> Endpoints { get; set; }
+    }
+}
