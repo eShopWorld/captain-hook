d001a46c09924d0ca17fd652298cc44d64aa68b7
dawidkeshop <63288056+dawidkeshop@users.noreply.github.com>
2020-06-22 15:12:55 +0200

    DTOs for API defined (#104)

---
+++ src/CaptainHook.Contract/EndpointDto.cs
@@ -0,0 +1,13 @@
+﻿namespace CaptainHook.Contract
+{
+    public class EndpointDto
+    {
+        public string Uri { get; set; }
+
+        public string HttpVerb { get; set; }
+
+        public string Selector { get; set; }
+
+        public AuthenticationDto Authentication{ get; set; }
+    }
+}
