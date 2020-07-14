81f50a7b5ce04885d31c2d1e1169ab3399264fe8
dawidkeshop <dkaliszewski@eshopworld.com>
2020-07-14 17:58:58 +0200

    Tests for MediatR handling and validation infrastructure

--- src/CaptainHook.Application/ApplicationModule.cs
+++ src/CaptainHook.Application/ApplicationModule.cs
@@ -1,40 +1,18 @@
-﻿using System.Reflection;
-using Autofac;
+﻿using Autofac;
 using CaptainHook.Application.Handlers.Subscribers;
 using CaptainHook.Application.Infrastructure;
 using CaptainHook.Application.RequestValidators;
-using FluentValidation;
-using MediatR;
 
 namespace CaptainHook.Application
 {
-    public class ApplicationModule : Autofac.Module
+    public class ApplicationModule : Module
     {
         protected override void Load(ContainerBuilder builder)
         {
-            builder
-                .RegisterType<Mediator>()
-                .As<IMediator>()
-                .InstancePerLifetimeScope();
+            var handlersAssembly = typeof(AddSubscriberRequestHandler).Assembly;
+            var validatorsAssembly = typeof(AddSubscriberRequestValidator).Assembly;
 
-            builder.Register<ServiceFactory>(context =>
-            {
-                var c = context.Resolve<IComponentContext>();
-                return t => c.Resolve(t);
-            });
-
-            builder
-                .RegisterAssemblyTypes(typeof(AddSubscriberRequestHandler).GetTypeInfo().Assembly)
-                .AsClosedTypesOf(typeof(IRequestHandler<,>))
-                .AsImplementedInterfaces();
-
-            var validatorsInAssembly = AssemblyScanner.FindValidatorsInAssembly(typeof(AddSubscriberRequestValidator).Assembly);
-            foreach (var validator in validatorsInAssembly)
-            {
-                builder.RegisterType(validator.ValidatorType).As(validator.InterfaceType);
-            }
-
-            builder.RegisterGeneric(typeof(ValidatorPipelineBehavior<,>)).As(typeof(IPipelineBehavior<,>));
+            builder.RegisterMediatorInfrastructure(handlersAssembly, validatorsAssembly);
         }
     }
 }
---
+++ src/Tests/CaptainHook.Application.Tests/ValidatorPipelineBehaviorTests.cs
@@ -0,0 +1,77 @@
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using Autofac;
+using CaptainHook.Application.Infrastructure;
+using CaptainHook.Domain.Results;
+using FluentAssertions;
+using FluentValidation;
+using MediatR;
+using Xunit;
+
+namespace CaptainHook.Application.Tests
+{
+    public class ValidatorPipelineBehaviorTests
+    {
+        private readonly IMediator _mediator;
+
+        public ValidatorPipelineBehaviorTests()
+        {
+            var thisAssembly = Assembly.GetExecutingAssembly();
+
+            var builder = new ContainerBuilder();
+            builder.RegisterMediatorInfrastructure(thisAssembly, thisAssembly);
+            var container = builder.Build();
+
+            _mediator = container.Resolve<IMediator>();
+        }
+
+        [Fact]
+        public async void For_invalid_request_should_call_validator_and_return_Error()
+        {
+            var request = new DivideRequest { Dividend = 6, Divisor = 0 };
+
+            var result =await _mediator.Send(request);
+
+            result.Should().BeOfType<EitherErrorOr<DivideResult>>().Which.IsError.Should().BeTrue();
+        }
+
+        [Fact]
+        public async void For_valid_request_should_call_validator_and_return_data()
+        {
+            var request = new DivideRequest { Dividend = 6, Divisor = 2 };
+
+            var result = await _mediator.Send(request);
+
+            result.Should().BeOfType<EitherErrorOr<DivideResult>>().Which.IsError.Should().BeFalse();
+        }
+
+
+        public class DivideRequest : IRequest<EitherErrorOr<DivideResult>>
+        {
+            public double Dividend { get; set; }
+            public double Divisor { get; set; }
+        }
+
+        public class DivideResult
+        {
+            public double Quotient { get; set; }
+        }
+
+        private class DivideRequestHandler : IRequestHandler<DivideRequest, EitherErrorOr<DivideResult>>
+        {
+            public async Task<EitherErrorOr<DivideResult>> Handle(DivideRequest request, CancellationToken cancellationToken)
+            {
+                return new DivideResult { Quotient = request.Dividend / request.Divisor };
+            }
+        }
+
+        public class DivideRequestValidator : AbstractValidator<DivideRequest>
+        {
+            public DivideRequestValidator()
+            {
+                RuleFor(r => r.Divisor).NotEqual(0);
+            }
+        }
+    }
+}
