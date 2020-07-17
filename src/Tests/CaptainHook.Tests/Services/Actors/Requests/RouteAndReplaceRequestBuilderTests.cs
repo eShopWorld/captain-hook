30d7a647a6cd877f8399182359d47f0406d50a7f
Michal Szymczak <mszymczak@eshopworld.com>
2020-07-17 13:33:57 +0200

    #75636 - add validation into Route and Replace logic

--- src/Tests/CaptainHook.Tests/Services/Actors/Requests/RouteAndReplaceRequestBuilderTests.cs
+++ src/Tests/CaptainHook.Tests/Services/Actors/Requests/RouteAndReplaceRequestBuilderTests.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using CaptainHook.Common.Configuration;
+using CaptainHook.Common.Telemetry.Message;
 using CaptainHook.EventHandlerActor.Handlers.Requests;
 using CaptainHook.Tests.Builders;
 using Eshopworld.Core;
 using Eshopworld.Tests.Core;
 using FluentAssertions;
+using FluentValidation;
+using FluentValidation.Results;
 using Moq;
 using Xunit;
 
@@ -13,14 +16,126 @@ namespace CaptainHook.Tests.Services.Actors.Requests
 {
     public class RouteAndReplaceRequestBuilderTests
     {
+        private readonly Mock<IValidator<WebhookConfig>> _validatorMock = new Mock<IValidator<WebhookConfig>>();
+
+        private readonly Mock<IBigBrother> _bigBrotherMock = new Mock<IBigBrother>();
+
+        private readonly RouteAndReplaceRequestBuilder _builder;
+
+        public RouteAndReplaceRequestBuilderTests()
+        {
+            _builder = new RouteAndReplaceRequestBuilder(_bigBrotherMock.Object, _validatorMock.Object);
+        }
+
         [IsUnit]
         [Theory]
         [MemberData(nameof(UriDataRouteAndReplace))]
         public void UriConstructionRouteAndReplaceTests(WebhookConfig config, string payload, string expectedUri)
         {
-            var uri = new RouteAndReplaceRequestBuilder(Mock.Of<IBigBrother>()).BuildUri(config, payload);
+            _validatorMock.Setup(v => v.Validate(It.IsAny<WebhookConfig>())).Returns(new ValidationResult());
+
+            var uri = _builder.BuildUri(config, payload);
 
             uri.Should().BeEquivalentTo(new Uri(expectedUri));
+        }
+
+        [IsUnit]
+        [Fact]
+        public void BuildUri_PayloadDoesNotContainReplaceValues_UnroutablePublishedAndNullReturned()
+        {
+            var config = new WebhookConfigBuilder()
+                .AddWebhookRequestRule(ruleBuilder => ruleBuilder
+                    .WithSource(source => source
+                        .AddReplace("selector", "$.TenantCode")
+                        .AddReplace("orderCode", "$.OrderCode"))
+                    .WithDestination(ruleAction: RuleAction.RouteAndReplace)
+                    .AddRoute(route => route
+                        .WithUri("https://blah.blah.{selector}.eshopworld.com/webhook/{orderCode}")
+                        .WithSelector("*")
+                        .WithNoAuthentication()))
+                .Create();
+            const string payload = "{\"BrandType\":\"Brand2\", \"TenantCode\":\"tenant1\"}";
+            _validatorMock.Setup(v => v.Validate(It.IsAny<WebhookConfig>())).Returns(new ValidationResult());
+
+            var uri = _builder.BuildUri(config, payload);
+
+            uri.Should().BeNull();
+            _bigBrotherMock.Verify(bb => bb.Publish(
+                    It.Is<UnroutableMessageEvent>(
+                        message => message.Message == "Error looking for $.OrderCode in the message payload"),
+                    It.IsAny<string>(),
+                    It.IsAny<string>(),
+                    It.IsAny<int>()),
+                Times.Once);
+        }
+
+        [IsUnit]
+        [Fact]
+        public void BuildUri_PayloadDoesNotContainAnyReplaceValues_UnroutablePublishedAndNullReturned()
+        {
+            var config = new WebhookConfigBuilder()
+                .AddWebhookRequestRule(ruleBuilder => ruleBuilder
+                    .WithSource(source => source
+                        .AddReplace("selector", "$.TenantCode")
+                        .AddReplace("orderCode", "$.OrderCode"))
+                    .WithDestination(ruleAction: RuleAction.RouteAndReplace)
+                    .AddRoute(route => route
+                        .WithUri("https://blah.blah.{selector}.eshopworld.com/webhook/{orderCode}")
+                        .WithSelector("*")
+                        .WithNoAuthentication()))
+                .Create();
+            const string payload = "{\"BrandType\":\"Brand2\"}";
+            _validatorMock.Setup(v => v.Validate(It.IsAny<WebhookConfig>())).Returns(new ValidationResult());
+
+            var uri = _builder.BuildUri(config, payload);
+
+            uri.Should().BeNull();
+            _bigBrotherMock.Verify(bb => bb.Publish(
+                    It.Is<UnroutableMessageEvent>(
+                        message => message.Message == "Error looking for $.TenantCode in the message payload"),
+                    It.IsAny<string>(),
+                    It.IsAny<string>(),
+                    It.IsAny<int>()),
+                Times.Exactly(2)); // 2 times because 1 for selector and the other for replacement dictionary
+            _bigBrotherMock.Verify(bb => bb.Publish(
+                    It.Is<UnroutableMessageEvent>(
+                        message => message.Message == "Error looking for $.OrderCode in the message payload"),
+                    It.IsAny<string>(),
+                    It.IsAny<string>(),
+                    It.IsAny<int>()),
+                Times.Once); // 1 time only for replacement dictionary
+        }
+
+        [IsUnit]
+        [Fact]
+        public void BuildUri_InvalidConfig_ReturnsNull()
+        {
+            var validationResult = new ValidationResult(new[] { new ValidationFailure("a", "error") });
+            _validatorMock.Setup(v => v.Validate(It.IsAny<WebhookConfig>())).Returns(validationResult);
+
+            var uri = _builder.BuildUri(new WebhookConfig(), "payload-payload");
+
+            uri.Should().BeNull("because failure to validate should result in null uri");
+        }
+
+        [IsUnit]
+        [Fact]
+        public void BuildUri_InvalidConfig_PublishesMessage()
+        {
+            var validationResult = new ValidationResult(new[] { new ValidationFailure("a", "error") });
+            _validatorMock.Setup(v => v.Validate(It.IsAny<WebhookConfig>())).Returns(validationResult);
+            
+            _builder.BuildUri(new WebhookConfigBuilder().WithType("type-for-test").Create(), "payload-payload");
+
+            _bigBrotherMock.Verify(bb => bb.Publish(
+                    It.Is<UnroutableMessageEvent>(
+                        message => message.EventType == "type-for-test" &&
+                                   message.SubscriberName == "type-for-test" &&
+                                   message.Message == "Validation errors for subscriber configuration: error"),
+                    It.IsAny<string>(),
+                    It.IsAny<string>(),
+                    It.IsAny<int>()),
+                Times.Once);
         }
 
         [IsUnit]
@@ -28,7 +143,7 @@ namespace CaptainHook.Tests.Services.Actors.Requests
         [MemberData(nameof(WebhookConfigData))]
         public void SelectWebhookConfigTests(WebhookConfig config, string payload, WebhookConfig expectedWebhookConfig)
         {
-            var actualConfig = new RouteAndReplaceRequestBuilder(Mock.Of<IBigBrother>()).SelectWebhookConfig(config, payload);
+            var actualConfig = _builder.SelectWebhookConfig(config, payload);
 
             actualConfig.Should().BeEquivalentTo(expectedWebhookConfig);
         }
