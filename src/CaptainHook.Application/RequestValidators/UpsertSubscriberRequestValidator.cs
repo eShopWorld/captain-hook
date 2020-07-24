96f989c3d51b311d1b5b80722c5540029a1c1e96
dawidkeshop <dkaliszewski@eshopworld.com>
2020-07-24 03:06:46 +0200

    Validation partially implemented

--- src/CaptainHook.Application/RequestValidators/UpsertSubscriberRequestValidator.cs
+++ src/CaptainHook.Application/RequestValidators/UpsertSubscriberRequestValidator.cs
@@ -1,7 +1,6 @@
 ﻿using CaptainHook.Application.Requests.Subscribers;
 using CaptainHook.Contract;
 using FluentValidation;
-using FluentValidation.Validators;
 
 namespace CaptainHook.Application.RequestValidators
 {
@@ -11,6 +10,19 @@ namespace CaptainHook.Application.RequestValidators
         {
             RuleFor(x => x.EventName).NotEmpty();
             RuleFor(x => x.SubscriberName).NotEmpty();
+            RuleFor(x => x.Endpoint).NotNull()
+                .SetValidator(new EndpointDtoValidator());
+        }
+    }
+
+    public class EndpointDtoValidator : AbstractValidator<EndpointDto>
+    {
+        public EndpointDtoValidator()
+        {
+            RuleFor(x => x.HttpVerb).NotEmpty();
+            RuleFor(x => x.Uri).NotEmpty();
+            RuleFor(x => x.Authentication).NotNull()
+                .SetValidator(new AuthenticationDtoValidator());
         }
     }
 
@@ -18,6 +30,21 @@ namespace CaptainHook.Application.RequestValidators
     {
         public AuthenticationDtoValidator()
         {
+            RuleFor(x => x.Type).NotEmpty();
+            RuleFor(x => x.ClientId).NotEmpty();
+            RuleFor(x => x.Uri).NotEmpty();
+            RuleFor(x => x.Scopes).NotEmpty();
+            RuleForEach(x => x.Scopes).NotEmpty();
+            RuleFor(x => x.ClientSecret).NotNull();
+        }
+
+        private class ClientSecretDtoValidator : AbstractValidator<ClientSecretDto>
+        {
+            public ClientSecretDtoValidator()
+            {
+                RuleFor(x => x.Name).NotEmpty();
+                RuleFor(x => x.Vault).NotEmpty();
+            }
         }
     }
 }
