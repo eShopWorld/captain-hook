eb872b7e16dbed6c25f53551a8a2c13dcbd8867b
dawidkeshop <dkaliszewski@eshopworld.com>
2020-07-24 10:15:29 +0200

    Small improvements in global test data

---
+++ src/Tests/CaptainHook.TestsInfrastructure/TestsData/InvalidUris.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace CaptainHook.TestsInfrastructure.TestsData
+{
+    public class InvalidUris : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] { "not-a-uri" };
+            yield return new object[] { "https:/invalid.com" };
+            yield return new object[] { "https://.com" };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
