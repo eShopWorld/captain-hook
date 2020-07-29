5d8ce3f7f09fe75e7d8a605c0bbcdedf9fd3fd05
Nikhil Khandelwal <nkhandelwal@eshopworld.com>
2020-07-29 13:29:55 +0100

    Added appsettings.json to CaptainHook.Tests for CI and Development

---
+++ src/Tests/CaptainHook.Tests/Configuration/TestsConfig.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CaptainHook.Tests.Configuration
+{
+    public class TestsConfig
+    {
+        public TestsConfig()
+        {
+            var config = Eshopworld.DevOps.EswDevOpsSdk.BuildConfiguration(); // don't load whole kv
+            var kvConfig = new ConfigurationBuilder().AddAzureKeyVault(
+                config["KeyVaultUrl"],
+                new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(new AzureServiceTokenProvider().KeyVaultTokenCallback)),
+                new DefaultKeyVaultSecretManager()).Build();
+
+            this.ServiceBusConnectionString = kvConfig["CaptainHook:ServiceBusConnectionString"]; // KV
+            this.StsClientSecret = kvConfig["TestConfig:Authentication:ClientSecret"]; // KV
+            
+            this.InstrumentationKey = config["InstrumentationKey"]; // AS
+            this.SubscriptionId = config["AzureSubscriptionId"]; // AS
+            this.PeterPanUrlBase = config["TestConfig:PeterPanBaseUrl"]; // AS
+            this.StsClientId = config["TestConfig:Authentication:StsClientId"]; // AS
+        }
+        public String InstrumentationKey { get; set; }
+        public String ServiceBusConnectionString { get; set; }
+        public String SubscriptionId { get; set; }
+        public String PeterPanUrlBase { get; set; }
+        public String StsClientSecret { get; set; }
+        public String StsClientId { get; set; }
+    }
+}
