4d929478e056a4f8e0894cfc47957ffff1fbe9c7
dawidkeshop <dkaliszewski@eshopworld.com>
2020-08-03 00:15:20 +0200

    Fixed IoC in Application

--- src/CaptainHook.Application/ApplicationModule.cs
+++ src/CaptainHook.Application/ApplicationModule.cs
@@ -1,7 +1,11 @@
-﻿using Autofac;
+﻿using System;
+using Autofac;
+using CaptainHook.Application.Gateways;
 using CaptainHook.Application.Handlers.Subscribers;
 using CaptainHook.Application.Infrastructure;
 using CaptainHook.Application.Validators;
+using CaptainHook.Common;
+using Microsoft.ServiceFabric.Services.Remoting.Client;
 
 namespace CaptainHook.Application
 {
@@ -9,9 +13,12 @@ namespace CaptainHook.Application
     {
         protected override void Load(ContainerBuilder builder)
         {
+            var directorServiceUri = new Uri(ServiceNaming.DirectorServiceFullName);
+            var directorServiceClient = ServiceProxy.Create<IDirectorServiceRemoting>(directorServiceUri);
+            builder.RegisterInstance(directorServiceClient);
+
             var handlersAssembly = typeof(UpsertWebhookRequestHandler).Assembly;
             var validatorsAssembly = typeof(UpsertWebhookRequestValidator).Assembly;
-
             builder.RegisterMediatorInfrastructure(handlersAssembly, validatorsAssembly);
         }
     }
