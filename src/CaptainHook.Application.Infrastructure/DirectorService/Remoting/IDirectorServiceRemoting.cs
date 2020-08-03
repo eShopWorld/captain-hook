75856675eee2d6b95a93f5bead10d15fdcee657e
dawidkeshop <dkaliszewski@eshopworld.com>
2020-08-03 13:00:28 +0200

    Added comment to IDirectorServiceRemoting

--- src/CaptainHook.Application.Infrastructure/DirectorService/Remoting/IDirectorServiceRemoting.cs
+++ src/CaptainHook.Application.Infrastructure/DirectorService/Remoting/IDirectorServiceRemoting.cs
@@ -5,6 +5,13 @@ using Microsoft.ServiceFabric.Services.Remoting;
 
 namespace CaptainHook.Application.Infrastructure.DirectorService.Remoting
 {
+    /// <summary>
+    /// Basic interface which allows to communicate Application with DirectorService.
+    /// </summary>
+    /// <remarks>
+    /// Data passed through remoting must be serializable, so we must not use entities and OperationResult here.
+    /// That's why SubscriberConfiguration is passed and enums are received.
+    /// </remarks>
     public interface IDirectorServiceRemoting : IService
     {
         Task<RequestReloadConfigurationResult> RequestReloadConfigurationAsync();
