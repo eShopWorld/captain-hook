ab73074d8446a250535880341d2854ac047b57b4
Antonino Parisi <tabman83@gmail.com>
2020-08-05 15:31:34 +0100

    #78979 Subscriber PUT endpoint and contract (#164)
    * #78979 Subscriber PUT endpoint and contract
    
    * Address PR comments
    
    * Restore subscriber name for put subscriber request
    
    * Update created URI for PUT request
    
    * Remove subscriber name from subscriber dto

---
+++ src/CaptainHook.Application/Requests/Subscribers/UpsertSubscriberRequest.cs
@@ -0,0 +1,20 @@
+﻿using CaptainHook.Contract;
+using CaptainHook.Domain.Results;
+using MediatR;
+
+namespace CaptainHook.Application.Requests.Subscribers
+{
+    public class UpsertSubscriberRequest : IRequest<OperationResult<EndpointDto>>
+    {
+        public string EventName { get; }
+        public string SubscriberName { get; }
+        public SubscriberDto Subscriber { get; }
+
+        public UpsertSubscriberRequest(string eventName, string subscriberName, SubscriberDto dto)
+        {
+            EventName = eventName;
+            SubscriberName = subscriberName;
+            Subscriber = dto;
+        }
+    }
+}
