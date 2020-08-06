3423d792b6c84e7bf3fb53a2c6d7dce29595172f
Antonino Parisi <aparisi@eshopworld.com>
2020-08-06 12:26:48 +0200

    Add PUT subscriber validators and unit tests

---
+++ src/CaptainHook.Application/Validators/Dtos/SubscriberDtoValidator.cs
@@ -0,0 +1,17 @@
+﻿using CaptainHook.Application.Validators.Common;
+using CaptainHook.Contract;
+using FluentValidation;
+
+namespace CaptainHook.Application.Validators.Dtos
+{
+    public class SubscriberDtoValidator : AbstractValidator<SubscriberDto>
+    {
+        public SubscriberDtoValidator()
+        {
+            CascadeMode = CascadeMode.StopOnFirstFailure;
+
+            RuleFor(x => x.Webhooks).NotEmpty()
+                .SetValidator(new WebhooksDtoValidator());
+        }
+    }
+}
---
+++ src/CaptainHook.Application/Validators/Dtos/WebhooksDtoValidator.cs
@@ -0,0 +1,44 @@
+﻿using CaptainHook.Contract;
+using FluentValidation;
+using Newtonsoft.Json.Linq;
+
+namespace CaptainHook.Application.Validators.Dtos
+{
+    public class WebhooksDtoValidator : AbstractValidator<WebhooksDto>
+    {
+        private static JObject _jObject = new JObject();
+
+        public WebhooksDtoValidator()
+        {
+            CascadeMode = CascadeMode.StopOnFirstFailure;
+
+            RuleFor(x => x.Endpoints).NotEmpty()
+                .Must(list => list.Count >= 1).WithMessage("Webhooks list must contain at list one endpoint");
+
+            RuleForEach(x => x.Endpoints)
+                .SetValidator(new EndpointDtoValidator());
+
+            RuleFor(x => x.SelectionRule).NotEmpty()
+                .Must(BeValidJsonPathExpression);
+        }
+
+        private bool BeValidJsonPathExpression(string selectionRule)
+        {
+            if(!selectionRule.StartsWith('$'))
+            {
+                return false;
+            }
+
+            try
+            {
+                _jObject.SelectToken(selectionRule, false);
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
