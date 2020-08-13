3b3e365403ea4c970527bf19d83580d877f3ff40
dawidkeshop <dkaliszewski@eshopworld.com>
2020-08-13 10:52:32 +0200

    DeleteWebhookRequest created

---
+++ src/CaptainHook.Application/Requests/Subscribers/DeleteWebhookRequest.cs
@@ -0,0 +1,18 @@
+﻿using CaptainHook.Contract;
+using CaptainHook.Domain.Results;
+using MediatR;
+
+namespace CaptainHook.Application.Requests.Subscribers
+{
+    public class DeleteWebhookRequest : IRequest<OperationResult<EndpointDto>>
+    {
+        public string EventName { get; }
+        public string SubscriberName { get; }
+
+        public DeleteWebhookRequest(string eventName, string subscriberName)
+        {
+            EventName = eventName;
+            SubscriberName = subscriberName;
+        }
+    }
+}
