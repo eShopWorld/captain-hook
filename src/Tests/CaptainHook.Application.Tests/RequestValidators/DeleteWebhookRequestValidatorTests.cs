8b91bcea250249fecfae9e487f2e618f97cb3ea8
dawidkeshop <dkaliszewski@eshopworld.com>
2020-08-13 11:46:56 +0200

    Tests for DeleteWebhookRequestValidator

---
+++ src/Tests/CaptainHook.Application.Tests/RequestValidators/DeleteWebhookRequestValidatorTests.cs
@@ -0,0 +1,47 @@
+﻿using CaptainHook.Application.Requests.Subscribers;
+using CaptainHook.Application.Validators;
+using CaptainHook.TestsInfrastructure.TestsData;
+using Eshopworld.Tests.Core;
+using FluentAssertions;
+using FluentValidation.TestHelper;
+using Xunit;
+
+namespace CaptainHook.Application.Tests.RequestValidators
+{
+    public class DeleteWebhookRequestValidatorTests
+    {
+        private readonly DeleteWebhookRequestValidator _validator = new DeleteWebhookRequestValidator();
+
+        [Fact, IsUnit]
+        public void When_RequestIsValid_Then_NoFailuresReturned()
+        {
+            var request = new DeleteWebhookRequest("event", "subscriber");
+
+            var result = _validator.TestValidate(request);
+
+            AssertionExtensions.Should(result.IsValid).BeTrue();
+        }
+
+        [Theory, IsUnit]
+        [ClassData(typeof(EmptyStrings))]
+        public void When_EventIsEmpty_Then_ValidationFails(string invalidString)
+        {
+            var request = new DeleteWebhookRequest(invalidString, "subscriber");
+
+            var result = _validator.TestValidate(request);
+
+            result.ShouldHaveValidationErrorFor(x => x.EventName);
+        }
+
+        [Theory, IsUnit]
+        [ClassData(typeof(EmptyStrings))]
+        public void When_SubscriberIsEmpty_Then_ValidationFails(string invalidString)
+        {
+            var request = new DeleteWebhookRequest("event", invalidString);
+
+            var result = _validator.TestValidate(request);
+
+            result.ShouldHaveValidationErrorFor(x => x.SubscriberName);
+        }
+    }
+}
