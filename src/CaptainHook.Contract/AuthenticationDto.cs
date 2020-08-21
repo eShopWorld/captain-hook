031b032697717e62931a517be9153475f22dbb59
Antonino Parisi <aparisi@eshopworld.com>
2020-08-22 01:20:14 +0200

    Add Authentication JSON converter

--- src/CaptainHook.Contract/AuthenticationDto.cs
+++ src/CaptainHook.Contract/AuthenticationDto.cs
@@ -1,9 +1,11 @@
-﻿using System.Runtime.Serialization;
+﻿using Newtonsoft.Json;
+using System.Runtime.Serialization;
 
 namespace CaptainHook.Contract
 {
     [KnownType(typeof(OidcAuthenticationDto))]
     [KnownType(typeof(BasicAuthenticationDto))]
+    [JsonConverter(typeof(AuthenticationDtoJsonConverter))]
     public abstract class AuthenticationDto
     {
         public string Type { get; set; }
---
+++ src/CaptainHook.Contract/AuthenticationDtoJsonConverter.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace CaptainHook.Contract
+{
+    public class AuthenticationDtoJsonConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return typeof(AuthenticationDto).IsAssignableFrom(objectType);
+        }
+
+        public override object ReadJson(JsonReader reader,
+            Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            JObject jObject = JObject.Load(reader);
+
+            // Using a nullable bool here in case "is_album" is not present on an item
+            string type = (string)jObject["type"];
+
+            AuthenticationDto item;
+
+            item = type?.ToLower() switch
+            {
+                "oidc" => new OidcAuthenticationDto(),
+                "basic" => new BasicAuthenticationDto(),
+                _ => null
+            };
+
+
+            serializer.Populate(jObject.CreateReader(), item);
+
+            return item;
+        }
+
+        public override bool CanWrite
+        {
+            get { return false; }
+        }
+
+        public override void WriteJson(JsonWriter writer,
+            object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
