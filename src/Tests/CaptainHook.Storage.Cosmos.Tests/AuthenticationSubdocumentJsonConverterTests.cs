5351f4f6385ba59f91b208bee46de1b763845bab
Antonino Parisi <aparisi@eshopworld.com>
2020-08-24 13:09:05 +0200

    Add tests for AuthenticationSubdocumentJsonConverter

--- src/Tests/CaptainHook.Storage.Cosmos.Tests/AuthenticationSubdocumentJsonConverterTests.cs
+++ src/Tests/CaptainHook.Storage.Cosmos.Tests/AuthenticationSubdocumentJsonConverterTests.cs
@@ -1,13 +1,14 @@
-﻿using CaptainHook.Contract;
+﻿using CaptainHook.Storage.Cosmos.Models;
 using Eshopworld.Tests.Core;
 using FluentAssertions;
 using FluentAssertions.Execution;
 using Newtonsoft.Json;
+using System;
 using Xunit;
 
-namespace CaptainHook.Tests.Json
+namespace CaptainHook.Storage.Cosmos.Tests
 {
-    public class AuthenticationDtoJsonConverterTests
+    public class AuthenticationSubdocumentJsonConverterTests
     {
         [Fact]
         [IsUnit]
@@ -17,22 +18,22 @@ namespace CaptainHook.Tests.Json
                 ""type"": ""OIDC"",
                 ""clientId"": ""clientid"",
                 ""uri"": ""https://security.site.com/connect/token"",
-                ""clientSecretKeyName"": ""secret--key--name"",
+                ""secretName"": ""secret--key--name"",
                 ""scopes"": [
-                ""t.abc.client.api.all""
+                    ""t.abc.client.api.all""
                 ]
             }";
 
-            var result = JsonConvert.DeserializeObject<AuthenticationDto>(data, new AuthenticationDtoJsonConverter());
+            var result = JsonConvert.DeserializeObject<AuthenticationSubdocument>(data, new AuthenticationSubdocumentJsonConverter());
 
             using (new AssertionScope())
             {
-                result.AuthenticationType.Should().Be(OidcAuthenticationDto.Type);
-                result.Should().BeOfType<OidcAuthenticationDto>();
+                result.AuthenticationType.Should().Be(OidcAuthenticationSubdocument.Type);
+                result.Should().BeOfType<OidcAuthenticationSubdocument>();
 
-                var oidcAuth = result as OidcAuthenticationDto;
+                var oidcAuth = result as OidcAuthenticationSubdocument;
                 oidcAuth.ClientId.Should().Be("clientid");
-                oidcAuth.ClientSecretKeyName.Should().Be("secret--key--name");
+                oidcAuth.SecretName.Should().Be("secret--key--name");
                 oidcAuth.Uri.Should().Be("https://security.site.com/connect/token");
                 oidcAuth.Scopes.Should().BeEquivalentTo("t.abc.client.api.all");
             }
@@ -48,14 +49,14 @@ namespace CaptainHook.Tests.Json
                 ""password"": ""norris""
             }";
 
-            var result = JsonConvert.DeserializeObject<AuthenticationDto>(data, new AuthenticationDtoJsonConverter());
+            var result = JsonConvert.DeserializeObject<AuthenticationSubdocument>(data, new AuthenticationSubdocumentJsonConverter());
 
             using (new AssertionScope())
             {
-                result.AuthenticationType.Should().Be(BasicAuthenticationDto.Type);
-                result.Should().BeOfType<BasicAuthenticationDto>();
-
-                var basicAuth = result as BasicAuthenticationDto;
+                result.AuthenticationType.Should().Be(BasicAuthenticationSubdocument.Type);
+                result.Should().BeOfType<BasicAuthenticationSubdocument>();
+                
+                var basicAuth = result as BasicAuthenticationSubdocument;
                 basicAuth.Username.Should().Be("chuck");
                 basicAuth.Password.Should().Be("norris");
             }
@@ -79,9 +80,9 @@ namespace CaptainHook.Tests.Json
                 ""password"": ""norris""
             }}";
 
-            var result = JsonConvert.DeserializeObject<AuthenticationDto>(data, new AuthenticationDtoJsonConverter());
+            Action act = () => JsonConvert.DeserializeObject<AuthenticationSubdocument>(data, new AuthenticationSubdocumentJsonConverter());
 
-            result.Should().BeNull();
+            act.Should().Throw<InvalidOperationException>();
         }
     }
 }
