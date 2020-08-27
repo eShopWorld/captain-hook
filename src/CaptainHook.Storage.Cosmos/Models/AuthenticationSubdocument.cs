1ce887cb0b2ea5d54c51bcb5c5bc5acf74e4d58f
Antonino Parisi <aparisi@eshopworld.com>
2020-08-27 17:46:04 +0200

    #82398 Fix Cosmos DB serialization

--- src/CaptainHook.Storage.Cosmos/Models/AuthenticationSubdocument.cs
+++ src/CaptainHook.Storage.Cosmos/Models/AuthenticationSubdocument.cs
@@ -1,10 +1,13 @@
 ﻿using Newtonsoft.Json;
+using System.Runtime.Serialization;
 
 namespace CaptainHook.Storage.Cosmos.Models
 {
     /// <summary>
     /// Authentication model in cosmos db
     /// </summary>
+    [KnownType(typeof(OidcAuthenticationSubdocument))]
+    [KnownType(typeof(BasicAuthenticationSubdocument))]
     internal abstract class AuthenticationSubdocument
     {
         /// <summary>
