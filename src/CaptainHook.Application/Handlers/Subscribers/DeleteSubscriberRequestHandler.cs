495c52b0657746476c02474a0b6a52619dd74019
dawidkeshop <dkaliszewski@eshopworld.com>
2020-09-01 15:10:20 +0200

    Test passes

--- src/CaptainHook.Application/Handlers/Subscribers/DeleteSubscriberRequestHandler.cs
+++ src/CaptainHook.Application/Handlers/Subscribers/DeleteSubscriberRequestHandler.cs
@@ -2,7 +2,6 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 using CaptainHook.Application.Infrastructure.DirectorService;
-using CaptainHook.Application.Infrastructure.Mappers;
 using CaptainHook.Application.Requests.Subscribers;
 using CaptainHook.Domain.Errors;
 using CaptainHook.Domain.Repositories;
@@ -23,25 +22,22 @@ namespace CaptainHook.Application.Handlers.Subscribers
 
         private readonly ISubscriberRepository _subscriberRepository;
         private readonly IDirectorServiceProxy _directorService;
-        private readonly IEntityToDtoMapper _entityToDtoMapper;
         private readonly TimeSpan[] _retrySleepDurations;
 
         public DeleteSubscriberRequestHandler(
             ISubscriberRepository subscriberRepository,
             IDirectorServiceProxy directorService,
-            IEntityToDtoMapper entityToDtoMapper,
             TimeSpan[] sleepDurations = null)
         {
             _subscriberRepository = subscriberRepository ?? throw new ArgumentNullException(nameof(subscriberRepository));
             _directorService = directorService ?? throw new ArgumentNullException(nameof(directorService));
-            _entityToDtoMapper = entityToDtoMapper ?? throw new ArgumentNullException(nameof(entityToDtoMapper));
             _retrySleepDurations = sleepDurations?.SafeFastNullIfEmpty() ?? DefaultRetrySleepDurations;
         }
 
         public async Task<OperationResult<bool>> Handle(DeleteSubscriberRequest request, CancellationToken cancellationToken)
         {
             var executionResult = await Policy
-                .HandleResult<OperationResult<bool>>(result => result.Error is CannotUpdateEntityError)
+                .HandleResult<OperationResult<bool>>(result => result.Error is CannotDeleteEntityError)
                 .WaitAndRetryAsync(_retrySleepDurations)
                 .ExecuteAsync(() => DeleteSubscriberAsync(request));
 
--- src/Tests/CaptainHook.Application.Tests/Handlers/Subscribers/DeleteSubscriberRequestHandlerTests.cs
+++ src/Tests/CaptainHook.Application.Tests/Handlers/Subscribers/DeleteSubscriberRequestHandlerTests.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using CaptainHook.Application.Handlers.Subscribers;
 using CaptainHook.Application.Infrastructure.DirectorService;
-using CaptainHook.Application.Infrastructure.Mappers;
 using CaptainHook.Application.Requests.Subscribers;
-using CaptainHook.Contract;
 using CaptainHook.Domain.Entities;
 using CaptainHook.Domain.Errors;
 using CaptainHook.Domain.Repositories;
@@ -25,7 +22,6 @@ namespace CaptainHook.Application.Tests.Handlers.Subscribers
     {
         private readonly Mock<ISubscriberRepository> _repositoryMock = new Mock<ISubscriberRepository>();
         private readonly Mock<IDirectorServiceProxy> _directorServiceMock = new Mock<IDirectorServiceProxy>();
-        private readonly Mock<IEntityToDtoMapper> _entityToDtoMapper = new Mock<IEntityToDtoMapper>();
 
         private static OidcAuthenticationEntity _authentication = new OidcAuthenticationEntity(
                 "captain-hook-id",
@@ -40,38 +36,6 @@ namespace CaptainHook.Application.Tests.Handlers.Subscribers
                .WithWebhook("https://blah.blah.eshopworld.com/default/", "POST", null, authentication: _authentication)
                .WithWebhook("https://blah.blah.eshopworld.com/webhook/", "POST", "selector", authentication: _authentication)
                .WithWebhook("https://blah.blah.eshopworld.com/other-webhook/", "POST", "non-deletable", authentication: _authentication);
-
-        private static readonly OidcAuthenticationDto _authenticationDto = new OidcAuthenticationDto
-        {
-            ClientId = "captain-hook-id",
-            ClientSecretKeyName = "kv-secret-name",
-            Uri = "https://blah-blah.sts.eshopworld.com",
-            Scopes = new List<string> { "scope1" }
-        };
-
-        private static readonly EndpointDto _firstEndpoint = new EndpointDto
-        {
-            Selector = null,
-            Uri = "https://blah.blah.eshopworld.com/default/",
-            HttpVerb = "POST",
-            Authentication = _authenticationDto
-        };
-
-        private static readonly EndpointDto _secondEndpoint = new EndpointDto
-        {
-            Selector = "selector",
-            Uri = "https://blah.blah.eshopworld.com/webhook/",
-            HttpVerb = "POST",
-            Authentication = _authenticationDto
-        };
-
-        private static readonly EndpointDto _thirdEndpoint = new EndpointDto
-        {
-            Selector = "non-deletable",
-            Uri = "https://blah.blah.eshopworld.com/other-webhook/",
-            HttpVerb = "POST",
-            Authentication = _authenticationDto
-        };
 
         private readonly DeleteSubscriberRequest _defaultRequest = new DeleteSubscriberRequest("event", "subscriber");
 
@@ -90,7 +54,6 @@ namespace CaptainHook.Application.Tests.Handlers.Subscribers
         private DeleteSubscriberRequestHandler Handler => new DeleteSubscriberRequestHandler(
             _repositoryMock.Object,
             _directorServiceMock.Object,
-            _entityToDtoMapper.Object,
             new[]
             {
                 TimeSpan.FromMilliseconds(10.0),
@@ -100,75 +63,18 @@ namespace CaptainHook.Application.Tests.Handlers.Subscribers
         [Fact, IsUnit]
         public async Task When_EndpointCanBeDeleted_Then_ChangesWillBeAppliedByDirectorServiceAndStoredInRepository()
         {
-            _entityToDtoMapper.Setup(x => x.MapSubscriber(It.IsAny<SubscriberEntity>()))
-                .Returns(new SubscriberDto
-                {
-                    Webhooks = new WebhooksDto
-                    {
-                        SelectionRule = "$.Test",
-                        Endpoints = new List<EndpointDto> { _firstEndpoint, _thirdEndpoint }
-                    }
-                });
-
             var result = await Handler.Handle(new DeleteSubscriberRequest("event", "subscriber"), CancellationToken.None);
 
             using var scope = new AssertionScope();
             result.IsError.Should().BeFalse();
             result.Data.Should().BeTrue();
 
-            var subscriberId = new SubscriberId("event", "Subscriber");
+            var subscriberId = new SubscriberId("event", "subscriber");
 
             _repositoryMock.Verify(x => x.GetSubscriberAsync(It.Is<SubscriberId>(id => id.Equals(subscriberId))), Times.Once);
             _directorServiceMock.Verify(x => x.DeleteReaderAsync(It.IsAny<SubscriberEntity>()), Times.Once);
             _repositoryMock.Verify(x => x.RemoveSubscriberAsync(It.Is<SubscriberId>(id => id.Equals(subscriberId))), Times.Once);
         }
-
-        //[Fact, IsUnit]
-        //public async Task When_DefaultEndpointCanBeDeleted_Then_ChangesWillBeAppliedByDirectorServiceAndStoredInRepository()
-        //{
-        //    _entityToDtoMapper.Setup(x => x.MapSubscriber(It.IsAny<SubscriberEntity>()))
-        //        .Returns(new SubscriberDto
-        //        {
-        //            Webhooks = new WebhooksDto
-        //            {
-        //                SelectionRule = "$.Test",
-        //                Endpoints = new List<EndpointDto> { _secondEndpoint, _thirdEndpoint }
-        //            }
-        //        });
-
-        //    var result = await Handler.Handle(new DeleteWebhookRequest("event", "subscriber", null), CancellationToken.None);
-
-        //    using var scope = new AssertionScope();
-        //    result.IsError.Should().BeFalse();
-        //    result.Data.Webhooks.Endpoints.Should().HaveCount(2);
-        //    result.Data.Webhooks.Endpoints.Should().OnlyContain(x => x.Selector != null);
-        //    _repositoryMock.Verify(x => x.GetSubscriberAsync(It.IsAny<SubscriberId>()), Times.Once);
-        //    _directorServiceMock.Verify(x => x.DeleteReaderAsync(It.IsAny<SubscriberEntity>()), Times.Once);
-        //    _repositoryMock.Verify(x => x.RemoveSubscriberAsync(It.IsAny<SubscriberEntity>()), Times.Once);
-        //}
-
-        //[Fact, IsUnit]
-        //public async Task When_EndpointDoesNotExist_Then_EndpointNotFoundInSubscriberErrorReturned()
-        //{
-        //    _entityToDtoMapper.Setup(x => x.MapSubscriber(It.IsAny<SubscriberEntity>()))
-        //        .Returns(new SubscriberDto
-        //        {
-        //            Webhooks = new WebhooksDto
-        //            {
-        //                SelectionRule = "$.Test",
-        //                Endpoints = new List<EndpointDto> { _firstEndpoint, _secondEndpoint, _thirdEndpoint }
-        //            }
-        //        });
-
-        //    var result = await Handler.Handle(new DeleteSubscriberRequest("event", "subscriber"), CancellationToken.None);
-
-        //    using var scope = new AssertionScope();
-        //    result.IsError.Should().BeTrue();
-        //    result.Error.Should().BeOfType<EndpointNotFoundInSubscriberError>();
-        //    _repositoryMock.Verify(x => x.GetSubscriberAsync(It.IsAny<SubscriberId>()), Times.Once);
-        //    _directorServiceMock.Verify(x => x.DeleteReaderAsync(It.IsAny<SubscriberEntity>()), Times.Never);
-        //    _repositoryMock.Verify(x => x.RemoveSubscriberAsync(It.IsAny<SubscriberEntity>()), Times.Never);
-        //}
 
         [Fact, IsUnit]
         public async Task When_SubscriberDoesNotExist_Then_EntityNotFoundErrorReturned()
@@ -186,41 +92,6 @@ namespace CaptainHook.Application.Tests.Handlers.Subscribers
             _repositoryMock.Verify(x => x.RemoveSubscriberAsync(It.IsAny<SubscriberId>()), Times.Never);
         }
 
-        //[Fact, IsUnit]
-        //public async Task When_SubscriberContainOnlyOneEndpoint_Then_CannotRemoveLastEndpointFromSubscriberErrorReturned()
-        //{
-        //    var subscriberEntity = new SubscriberBuilder()
-        //       .WithEvent("event")
-        //       .WithName("subscriber")
-        //       .WithWebhookSelectionRule("$.Test")
-        //       .WithWebhook("https://blah.blah.eshopworld.com/webhook/", "POST", "selector", authentication: _authentication)
-        //       .Create();
-        //    _repositoryMock.Setup(x => x.GetSubscriberAsync(It.Is<SubscriberId>(id => id.Equals(new SubscriberId("event", "subscriber")))))
-        //        .ReturnsAsync(subscriberEntity);
-        //    _repositoryMock.Setup(x => x.RemoveSubscriberAsync(It.Is<SubscriberEntity>(entity => entity.Webhooks.Endpoints.Count() == 1)))
-        //        .ReturnsAsync(subscriberEntity);
-        //    _directorServiceMock.Setup(x => x.DeleteReaderAsync(It.IsAny<SubscriberEntity>())).ReturnsAsync(true);
-
-        //    _entityToDtoMapper.Setup(x => x.MapSubscriber(It.IsAny<SubscriberEntity>()))
-        //        .Returns(new SubscriberDto
-        //        {
-        //            Webhooks = new WebhooksDto
-        //            {
-        //                SelectionRule = "$.Test",
-        //                Endpoints = new List<EndpointDto> { _thirdEndpoint }
-        //            }
-        //        });
-
-        //    var result = await Handler.Handle(_defaultRequest, CancellationToken.None);
-
-        //    using var scope = new AssertionScope();
-        //    result.IsError.Should().BeTrue();
-        //    result.Error.Should().BeOfType<CannotRemoveLastEndpointFromSubscriberError>();
-        //    _repositoryMock.Verify(x => x.GetSubscriberAsync(It.IsAny<SubscriberId>()), Times.Once);
-        //    _directorServiceMock.Verify(x => x.DeleteReaderAsync(It.IsAny<SubscriberEntity>()), Times.Never);
-        //    _repositoryMock.Verify(x => x.RemoveSubscriberAsync(It.IsAny<SubscriberEntity>()), Times.Never);
-        //}
-
         [Fact, IsUnit]
         public async Task When_DirectorServiceIsBusyReloading_Then_OperationFails()
         {
@@ -235,22 +106,6 @@ namespace CaptainHook.Application.Tests.Handlers.Subscribers
             _directorServiceMock.Verify(x => x.DeleteReaderAsync(It.IsAny<SubscriberEntity>()), Times.Once);
             _repositoryMock.Verify(x => x.RemoveSubscriberAsync(It.IsAny<SubscriberId>()), Times.Never);
         }
-
-        //[Fact, IsUnit]
-        //public async Task When_DirectorServiceFailsToUpdateReaderBecauseOfCreateError_Then_OperationFails()
-        //{
-        //    _directorServiceMock.Setup(x => x.DeleteReaderAsync(It.IsAny<SubscriberEntity>()))
-        //        .ReturnsAsync(new ReaderCreateError(new SubscriberEntity("subscriber")));
-
-        //    var result = await Handler.Handle(_defaultRequest, CancellationToken.None);
-
-        //    using var scope = new AssertionScope();
-        //    result.IsError.Should().BeTrue();
-        //    result.Error.Should().BeOfType<ReaderCreateError>();
-        //    _repositoryMock.Verify(x => x.GetSubscriberAsync(It.IsAny<SubscriberId>()), Times.Once);
-        //    _directorServiceMock.Verify(x => x.DeleteReaderAsync(It.IsAny<SubscriberEntity>()), Times.Once);
-        //    _repositoryMock.Verify(x => x.RemoveSubscriberAsync(It.IsAny<SubscriberEntity>()), Times.Never);
-        //}
 
         [Fact, IsUnit]
         public async Task When_DirectorServiceFailsToUpdateReaderBecauseOfDeleteError_Then_OperationFails()
