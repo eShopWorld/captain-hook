c85c71200f216e8ffc4542e04e1a3c448f168e3b
dawidkeshop <dkaliszewski@eshopworld.com>
2020-09-02 13:20:58 +0200

    TaskExtensions moved to a separate file

--- src/CaptainHook.Domain/Results/OperationResult.cs
+++ src/CaptainHook.Domain/Results/OperationResult.cs
@@ -40,17 +40,4 @@ namespace CaptainHook.Domain.Results
 
         public static implicit operator TData(OperationResult<TData> result) => result.Data;
     }
-
-    public static class TaskExtensions
-    {
-        public static async Task<OperationResult<TOutput>> Then<TInput, TOutput>(this Task<OperationResult<TInput>> task, Func<TInput, OperationResult<TOutput>> func)
-        {
-            return (await task).Then(func);
-        }
-
-        public static async Task<OperationResult<TOutput>> Then<TInput, TOutput>(this Task<OperationResult<TInput>> task, Func<TInput, Task<OperationResult<TOutput>>> func)
-        {
-            return await (await task).Then(func);
-        }
-    }
 }
