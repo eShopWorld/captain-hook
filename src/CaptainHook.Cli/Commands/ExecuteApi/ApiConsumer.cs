4d676d454008ce8b3f169b464f6fe1bceb7382ee
dawidkeshop <dkaliszewski@eshopworld.com>
2020-09-08 14:11:15 +0200

    ApiConsumer created

---
+++ src/CaptainHook.Cli/Commands/ExecuteApi/ApiConsumer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using CaptainHook.Domain.Results;
+
+namespace CaptainHook.Cli.Commands.ExecuteApi
+{
+    public class ApiConsumer
+    {
+        public OperationResult<HttpWebResponse> CallApi(IEnumerable<PutSubscriberRequest> requests)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/CaptainHook.Cli/Commands/ExecuteApi/PutSubscriberRequest.cs
@@ -0,0 +1,11 @@
+﻿using CaptainHook.Api.Client.Models;
+
+namespace CaptainHook.Cli.Commands.ExecuteApi
+{
+    public class PutSubscriberRequest
+    {
+        public string Event { get; set; }
+        public string Subscriber { get; set; }
+        public CaptainHookContractSubscriberDto SubscriberDto { get; set; }
+    }
+}
