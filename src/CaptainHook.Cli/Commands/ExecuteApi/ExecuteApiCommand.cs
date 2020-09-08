644901a1d0f1ee10759776643541d6ccc5e358bd
dawidkeshop <dkaliszewski@eshopworld.com>
2020-09-08 12:04:40 +0200

    Empty command

---
+++ src/CaptainHook.Cli/Commands/ExecuteApi/ExecuteApiCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using McMaster.Extensions.CommandLineUtils;
+
+namespace CaptainHook.Cli.Commands.ExecuteApi
+{
+    [Command("execution", Description = "Calls CaptainHook API to create/update subscribers"), HelpOption]
+    public class ExecuteApiCommand
+    {
+         public Task<int> OnExecuteAsync(CommandLineApplication app, IConsole console)
+         {
+             return Task.FromResult(1);
+         }
+    }
+}
