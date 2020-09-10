13550b5734a3b124388ed170d05e9bbbd461c4c6
Antonino Parisi <aparisi@eshopworld.com>
2020-09-10 11:28:36 +0100

    Add negative test

--- src/Tests/CaptainHook.Tests/Common/Configuration/KeyVault/SubscribersKeyVaultProviderTests.cs
+++ src/Tests/CaptainHook.Tests/Common/Configuration/KeyVault/SubscribersKeyVaultProviderTests.cs
@@ -27,7 +27,6 @@ namespace CaptainHook.Tests.Common.Configuration.KeyVault
                 ""1:callbackconfig:name"": ""callback"",
                 ""1:callbackconfig:type"": ""callback"",
                 ""1:callbackconfig:httpverb"": ""POST"",
-                ""1:callbackconfig:uri"": ""https://callback.eshopworld.com"",
                 ""1:callbackconfig:webhookrequestrules:1:source:path"": ""$.TenantCode"",
                 ""1:callbackconfig:webhookrequestrules:1:destination:ruleaction"": ""Route"",
                 ""1:callbackconfig:webhookrequestrules:1:routes:1:uri"": ""https://callback.eshopworld.com/route1"",
@@ -38,7 +37,7 @@ namespace CaptainHook.Tests.Common.Configuration.KeyVault
                 ""1:callbackconfig:webhookrequestrules:1:routes:1:authenticationconfig:clientsecret"": ""ClientSecret"",
                 ""1:callbackconfig:webhookrequestrules:1:routes:1:authenticationconfig:scopes:1"": ""scope1"",
                 ""1:callbackconfig:webhookrequestrules:1:routes:1:authenticationconfig:scopes:2"": ""scope2"",
-                ""1:callbackconfig:webhookrequestrules:1:routes:1:httpverb"": ""POST"",
+                ""1:callbackconfig:webhookrequestrules:1:routes:1:httpverb"": ""POST""
             }}";
 
             var provider = new SubscribersKeyVaultProvider(CreateLoaderMock(configurationData));
@@ -51,7 +50,6 @@ namespace CaptainHook.Tests.Common.Configuration.KeyVault
                     .WithType("event1")
                     .WithName("callback")
                     .WithSubscriberName("callback")
-                    .WithUri("https://callback.eshopworld.com")
                     .WithoutAuthentication()
                     .AddWebhookRequestRule(rule => rule
                         .WithSource(source => source.WithPath("$.TenantCode"))
@@ -67,6 +65,79 @@ namespace CaptainHook.Tests.Common.Configuration.KeyVault
 
             result.IsError.Should().BeFalse();
             result.Data.Should().BeEquivalentTo(expectedResult);
+        }
+
+        [Fact, IsUnit]
+        public void Load_WhenItemInArrayIsMissing_ReturnError()
+        {
+            var configurationData = @"{""event"":{
+                ""1:type"": ""event1"",
+                ""1:name"": ""event1"",
+                ""1:webhookconfig:uri"": ""https://blah.blah.eshopworld.com"",
+                ""1:webhookconfig:httpverb"": ""POST"",
+                ""1:webhookconfig:authenticationconfig:type"": ""basic"",
+                ""1:webhookconfig:authenticationconfig:username"": ""user"",
+                ""1:webhookconfig:authenticationconfig:password"": ""password"",
+                ""1:callbackconfig:name"": ""callback"",
+                ""1:callbackconfig:type"": ""callback"",
+                ""1:callbackconfig:httpverb"": ""POST"",
+                ""1:callbackconfig:webhookrequestrules:1:source:path"": ""$.TenantCode"",
+                ""1:callbackconfig:webhookrequestrules:1:destination:ruleaction"": ""Route"",
+                ""1:callbackconfig:webhookrequestrules:1:routes:1:uri"": ""https://callback.eshopworld.com/route1"",
+                ""1:callbackconfig:webhookrequestrules:1:routes:1:selector"": ""testsel"",
+                ""1:callbackconfig:webhookrequestrules:1:routes:1:authenticationconfig:type"": ""OIDC"",
+                ""1:callbackconfig:webhookrequestrules:1:routes:1:authenticationconfig:uri"": ""https://blah-blah.sts.eshopworld.com"",
+                ""1:callbackconfig:webhookrequestrules:1:routes:1:authenticationconfig:clientid"": ""ClientId"",
+                ""1:callbackconfig:webhookrequestrules:1:routes:1:authenticationconfig:clientsecret"": ""ClientSecret"",
+                ""1:callbackconfig:webhookrequestrules:1:routes:1:authenticationconfig:scopes:1"": ""scope1"",
+                ""1:callbackconfig:webhookrequestrules:1:routes:1:authenticationconfig:scopes:2"": ""scope2"",
+                ""1:callbackconfig:webhookrequestrules:1:routes:1:httpverb"": ""POST"",
+                ""1:callbackconfig:webhookrequestrules:1:routes:3:uri"": ""https://callback.eshopworld.com/route3"",
+                ""1:callbackconfig:webhookrequestrules:1:routes:3:selector"": ""testsel3"",
+                ""1:callbackconfig:webhookrequestrules:1:routes:3:authenticationconfig:type"": ""OIDC"",
+                ""1:callbackconfig:webhookrequestrules:1:routes:3:authenticationconfig:uri"": ""https://blah-blah.sts.eshopworld.com"",
+                ""1:callbackconfig:webhookrequestrules:1:routes:3:authenticationconfig:clientid"": ""ClientId"",
+                ""1:callbackconfig:webhookrequestrules:1:routes:3:authenticationconfig:clientsecret"": ""ClientSecret"",
+                ""1:callbackconfig:webhookrequestrules:1:routes:3:authenticationconfig:scopes:1"": ""scope1"",
+                ""1:callbackconfig:webhookrequestrules:1:routes:3:authenticationconfig:scopes:2"": ""scope2"",
+                ""1:callbackconfig:webhookrequestrules:1:routes:3:httpverb"": ""POST""
+            }}";
+
+            var provider = new SubscribersKeyVaultProvider(CreateLoaderMock(configurationData));
+            var result = provider.Load("keyvault");
+
+            var expectedResult = new SubscriberConfigurationBuilder()
+                .WithType("event1")
+                .WithName(null)
+                .WithCallback(callback => callback
+                    .WithType("event1")
+                    .WithName("callback")
+                    .WithSubscriberName("callback")
+                    .WithoutAuthentication()
+                    .AddWebhookRequestRule(rule => rule
+                        .WithSource(source => source.WithPath("$.TenantCode"))
+                        .WithDestination(ruleAction: RuleAction.Route)
+                        .AddRoute(route => route
+                            .WithUri("https://callback.eshopworld.com/route1")
+                            .WithSelector("testsel")
+                            .WithHttpVerb("POST")
+                            .WithOidcAuthentication())
+                        .AddRoute(route => route
+                            .WithUri("https://callback.eshopworld.com/route2")
+                            .WithSelector("testsel2")
+                            .WithHttpVerb("POST")
+                            .WithOidcAuthentication())
+                        .AddRoute(route => route
+                            .WithUri("https://callback.eshopworld.com/route3")
+                            .WithSelector("testsel3")
+                            .WithHttpVerb("POST")
+                            .WithOidcAuthentication())
+                ))
+                .AsMainConfiguration()
+                .Create();
+
+            result.IsError.Should().BeTrue();
+            result.Error.Should().BeOfType<KeyVaultConfigurationError>().Which.Failures.Should().HaveCount(1);
         }
 
         [Fact, IsUnit]
