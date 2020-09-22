7592999ebcce3440283d644411a640fac6f6ee97
Nikhil Khandelwal <nkhandelwal@eshopworld.com>
2020-09-22 18:30:50 +0100

    More and elaborate tests.

--- src/Tests/Platform.Eda.Cli.Tests/Commands/ConfigureEda/ConfigureEdaCommandTests.cs
+++ src/Tests/Platform.Eda.Cli.Tests/Commands/ConfigureEda/ConfigureEdaCommandTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Abstractions.TestingHelpers;
 using System.Net;
 using System.Net.Http;
 using System.Threading;
@@ -7,6 +8,7 @@ using System.Threading.Tasks;
 using CaptainHook.Api.Client;
 using CaptainHook.Api.Client.Models;
 using CaptainHook.Cli.Tests;
+using Eshopworld.Tests.Core;
 using FluentAssertions;
 using Microsoft.Rest;
 using Moq;
@@ -18,20 +20,21 @@ namespace Platform.Eda.Cli.Tests.Commands.ConfigureEda
 {
     public class ConfigureEdaCommandTests : CliTestBase
     {
+        internal const string MockCurrentDirectory = @"Z:\Sample\";
         private readonly ConfigureEdaCommand _configureEdaCommand;
         private readonly Mock<ICaptainHookClient> _mockCaptainHookClient;
 
         public ConfigureEdaCommandTests(ITestOutputHelper output) : base(output)
         {
             _mockCaptainHookClient = new Mock<ICaptainHookClient>();
-            _configureEdaCommand = new ConfigureEdaCommand(SubscribersDirectoryProcessorTests.GetMockFileSystem(), _mockCaptainHookClient.Object);
+            _configureEdaCommand = new ConfigureEdaCommand(new MockFileSystem(GetMockFiles1(), MockCurrentDirectory), _mockCaptainHookClient.Object);
         }
 
-        [Fact]
-        public async Task OnExecuteAsync_WhenRequestAccepted_Returns0()
+        [Fact, IsUnit]
+        public async Task OnExecuteAsync_WhenSingleFileRequestAccepted_Returns0()
         {
             // Arrange
-            _configureEdaCommand.InputFolderPath = "TestFiles";
+            _configureEdaCommand.InputFolderPath = SubscribersDirectoryProcessorTests.MockCurrentDirectory;
             _configureEdaCommand.NoDryRun = true;
             var response = new HttpOperationResponse<object>
             {
@@ -53,14 +56,62 @@ namespace Platform.Eda.Cli.Tests.Commands.ConfigureEda
             var result = await _configureEdaCommand.OnExecuteAsync(Application, Console);
 
             // Assert
+            Output.SplitLines().Should()
+                .Contain(
+                    $@"Reading files from folder: '{SubscribersDirectoryProcessorTests.MockCurrentDirectory}' to be run against CI environment")
+                .And.Contain("File 'sample1.json' has been found")
+                .And.Contain("Starting to run configuration against Captain Hook API")
+                .And.Contain("File 'sample1.json' has been processed successfully")
+                .And.Contain("Processing finished");
+
             result.Should().Be(0);
         }
 
-        [Fact]
+        [Fact, IsUnit]
+        public async Task OnExecuteAsync_WhenMultipleSubdirectoriesRequestAccepted_Returns0()
+        {
+            // Arrange
+            var configureEdaCommand = new ConfigureEdaCommand(new MockFileSystem(GetMockFiles2(), MockCurrentDirectory), _mockCaptainHookClient.Object);
+            configureEdaCommand.InputFolderPath = MockCurrentDirectory;
+            configureEdaCommand.NoDryRun = true;
+            var response = new HttpOperationResponse<object>
+            {
+                Response = new HttpResponseMessage(HttpStatusCode.Accepted)
+                {
+                    Content = new StringContent("Request accepted.")
+                }
+            };
+
+            _mockCaptainHookClient.Setup(client =>
+                client.PutSuscriberWithHttpMessagesAsync(
+                    It.IsAny<string>(),
+                    It.IsAny<string>(),
+                    It.IsAny<CaptainHookContractSubscriberDto>(),
+                    null,
+                    CancellationToken.None)).Returns(Task.FromResult(response));
+
+            // Act
+            var result = await configureEdaCommand.OnExecuteAsync(Application, Console);
+
+            // Assert
+            Output.SplitLines().Should()
+                .Contain(
+                    $@"Reading files from folder: '{MockCurrentDirectory}' to be run against CI environment")
+                .And.Contain("File 'sample1.json' has been found")
+                .And.Contain(@"File 'subdir\sample2.json' has been found")
+                .And.Contain("Starting to run configuration against Captain Hook API")
+                .And.Contain("File 'sample1.json' has been processed successfully")
+                .And.Contain(@"File 'subdir\sample2.json' has been processed successfully")
+                .And.Contain("Processing finished");
+
+            result.Should().Be(0);
+        }
+
+        [Fact, IsUnit]
         public async Task OnExecuteAsync_WhenRequestNotAccepted_Returns2()
         {
             // Arrange
-            _configureEdaCommand.InputFolderPath = "TestFiles";
+            _configureEdaCommand.InputFolderPath = SubscribersDirectoryProcessorTests.MockCurrentDirectory;
             _configureEdaCommand.NoDryRun = true;
             var response = new HttpOperationResponse<object>
             {
@@ -70,7 +121,6 @@ namespace Platform.Eda.Cli.Tests.Commands.ConfigureEda
                 }
             };
 
-            // Act
             _mockCaptainHookClient.Setup(client =>
                 client.PutSuscriberWithHttpMessagesAsync(
                     It.IsAny<string>(),
@@ -79,13 +129,23 @@ namespace Platform.Eda.Cli.Tests.Commands.ConfigureEda
                     null,
                     CancellationToken.None)).Returns(Task.FromResult(response));
 
-            // Assert
+            // Act
             var result = await _configureEdaCommand.OnExecuteAsync(Application, Console);
+
+            // Assert
+            Output.SplitLines().Should()
+                .Contain(
+                    $@"Reading files from folder: '{SubscribersDirectoryProcessorTests.MockCurrentDirectory}' to be run against CI environment")
+                .And.Contain("File 'sample1.json' has been found")
+                .And.Contain("Starting to run configuration against Captain Hook API")
+                .And.NotContain("File 'sample1.json' has been processed successfully")
+                .And.Contain("Error when processing 'sample1.json':")
+                .And.Contain("Status code: 409");
             result.Should().Be(2);
         }
 
-        [Fact]
-        public async Task OnExecuteAsync_WhenInputDirectoryPathNull_Returns1()
+        [Fact, IsUnit]
+        public async Task OnExecuteAsync_WhenInputDirectoryPathNull_ThrowsException()
         {
             // Arrange
             _configureEdaCommand.InputFolderPath = null;
@@ -94,22 +154,117 @@ namespace Platform.Eda.Cli.Tests.Commands.ConfigureEda
             await Assert.ThrowsAsync<ArgumentNullException>(() => _configureEdaCommand.OnExecuteAsync(Application, Console));
         }
 
-        [Fact]
-        public async Task OnExecuteAsync_WhenInputNoDryRunFalse_ApiIsNotCalled()
+        [Fact, IsUnit]
+        public async Task OnExecuteAsync_WhenNoDryRunFalse_ApiIsNotCalled()
         {
             // Arrange
-            _configureEdaCommand.InputFolderPath = "TestFiles";
+            _configureEdaCommand.InputFolderPath = SubscribersDirectoryProcessorTests.MockCurrentDirectory;
             _configureEdaCommand.NoDryRun = false;
 
-            // Act - Assert;
-            var result = await _configureEdaCommand.OnExecuteAsync(Application, Console); 
-            _mockCaptainHookClient.Verify(client=> 
-                client.PutSuscriberWithHttpMessagesAsync(
-                    It.IsAny<string>(), 
-                    It.IsAny<string>(), 
-                    It.IsAny<CaptainHookContractSubscriberDto>(), 
-                    It.IsAny<Dictionary<string, List<string>>>(), 
+            // Act;
+            var result = await _configureEdaCommand.OnExecuteAsync(Application, Console);
+
+            // Assert
+            _mockCaptainHookClient.Verify(client =>
+            client.PutSuscriberWithHttpMessagesAsync(
+                    It.IsAny<string>(),
+                    It.IsAny<string>(),
+                    It.IsAny<CaptainHookContractSubscriberDto>(),
+                    It.IsAny<Dictionary<string, List<string>>>(),
                     It.IsAny<CancellationToken>()), Times.Never);
+
+            Output.SplitLines().Should()
+                .Contain(
+                    $@"Reading files from folder: '{SubscribersDirectoryProcessorTests.MockCurrentDirectory}' to be run against CI environment")
+                .And.Contain("File 'sample1.json' has been found")
+                .And.NotContain("Starting to run configuration against Captain Hook API")
+                .And.Contain("By default the CLI runs in 'dry-run' mode. If you want to run the configuration against Captain Hook API use the '--no-dry-run' switch")
+                .And.Contain("Processing finished");
+        }
+
+        public static Dictionary<string, MockFileData> GetMockFiles1()
+        {
+            var mockFiles = new Dictionary<string, MockFileData>
+            {
+                {
+                    "sample1.json", new MockFileData(@"
+{
+  ""subscriberName"": ""test-sub"",
+  ""eventName"": ""test-event"",
+  ""subscriber"": {
+    ""webhooks"": {
+      ""endpoints"": [
+        {
+          ""uri"": ""https://blah.blah/testing"",
+          ""authentication"": {
+            ""type"": ""Basic"",
+            ""username"": ""test"",
+            ""passwordKeyName"": ""AzureSubscriptionId""
+          },
+          ""httpVerb"": ""post""
+        }
+      ]
+    }
+  }
+}
+")
+                }
+            };
+            return mockFiles;
+        }
+
+        public static Dictionary<string, MockFileData> GetMockFiles2()
+        {
+            var mockFiles = new Dictionary<string, MockFileData>
+            {
+                {
+                    "sample1.json", new MockFileData(@"
+{
+  ""subscriberName"": ""test-sub"",
+  ""eventName"": ""test-event"",
+  ""subscriber"": {
+    ""webhooks"": {
+      ""endpoints"": [
+        {
+          ""uri"": ""https://blah.blah/testing"",
+          ""authentication"": {
+            ""type"": ""Basic"",
+            ""username"": ""test"",
+            ""passwordKeyName"": ""AzureSubscriptionId""
+          },
+          ""httpVerb"": ""post""
+        }
+      ]
+    }
+  }
+}
+")
+                },
+                {
+                    "subdir/sample2.json", new MockFileData(@"
+{
+  ""subscriberName"": ""test-sub2"",
+  ""eventName"": ""test-event2"",
+  ""subscriber"": {
+    ""webhooks"": {
+      ""endpoints"": [
+        {
+          ""uri"": ""https://blah.blah/testing2"",
+          ""authentication"": {
+            ""type"": ""Basic"",
+            ""username"": ""test2"",
+            ""passwordKeyName"": ""AzureSubscriptionId""
+          },
+          ""httpVerb"": ""post""
+        }
+      ]
+    }
+  }
+}
+")
+                }
+            };
+            return mockFiles;
         }
     }
 }
--- src/Tests/Platform.Eda.Cli.Tests/Commands/ConfigureEda/SubscribersDirectoryProcessorTests.cs
+++ src/Tests/Platform.Eda.Cli.Tests/Commands/ConfigureEda/SubscribersDirectoryProcessorTests.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Abstractions;
 using System.IO.Abstractions.TestingHelpers;
 using System.Linq;
+using CaptainHook.Api.Client.Models;
+using CaptainHook.Domain.Results;
+using Eshopworld.Tests.Core;
 using FluentAssertions;
 using FluentAssertions.Execution;
 using Platform.Eda.Cli.Commands.ConfigureEda;
+using Platform.Eda.Cli.Commands.ConfigureEda.Models;
 using Platform.Eda.Cli.Common;
 using Xunit;
 
@@ -12,55 +18,99 @@ namespace Platform.Eda.Cli.Tests.Commands.ConfigureEda
 {
     public class SubscribersDirectoryProcessorTests
     {
+        internal const string MockCurrentDirectory = @"Z:\Sample\";
         readonly SubscribersDirectoryProcessor _subscribersDirectoryProcessor;
 
         public SubscribersDirectoryProcessorTests()
         {
-            _subscribersDirectoryProcessor = new SubscribersDirectoryProcessor(GetMockFileSystem());
+            _subscribersDirectoryProcessor = new SubscribersDirectoryProcessor(new MockFileSystem(GetMockFiles1(), MockCurrentDirectory));
         }
 
-        public static MockFileSystem GetMockFileSystem()
-        {
-            return new MockFileSystem(GetMockFiles(), Environment.CurrentDirectory);
-        }
-
-        [Theory]
+        [Theory, IsUnit]
         [InlineData(null, typeof(ArgumentNullException))]
         [InlineData("", typeof(ArgumentException))]
-        public void ProcessDirectory_WhenPathEmptyOrNull_ThrowsException(string s, Type exceptionType)
+        public void ProcessDirectory_WhenPathEmptyOrNull_ThrowsException(string testFilesDirectoryPath, Type exceptionType)
         {
             Assert.Throws(exceptionType, () =>
-                _subscribersDirectoryProcessor.ProcessDirectory(s));
+                _subscribersDirectoryProcessor.ProcessDirectory(testFilesDirectoryPath));
         }
 
-        [Theory]
+        [Theory, IsUnit]
         [InlineData("a-nonexistent-path")]
-        public void ProcessDirectory_WithNonExistentPath_ReturnsError(string s)
+        public void ProcessDirectory_WithNonExistentPath_ReturnsError(string testFilesDirectoryPath)
         {
-            var result = _subscribersDirectoryProcessor.ProcessDirectory(s);
+            var result = _subscribersDirectoryProcessor.ProcessDirectory(testFilesDirectoryPath);
             result.IsError.Should().BeTrue();
             result.Error.Should().BeOfType<CliExecutionError>();
         }
 
-        [Theory]
-        [InlineData("TestFiles")]
-        public void ProcessDirectory_WithValidFile_ReturnsNonNullObject(string s)
+        [Theory, IsUnit]
+        [InlineData(MockCurrentDirectory)]
+        public void ProcessDirectory_WithValidFile_ReturnsValidObject(string testFilesDirectoryPath)
         {
-            var result = _subscribersDirectoryProcessor.ProcessDirectory(s);
+            // Act
+            var result = _subscribersDirectoryProcessor.ProcessDirectory(testFilesDirectoryPath);
 
-            using (new AssertionScope())
-            {
-                result.IsError.Should().BeFalse();
-                result.Data.Should().HaveCount(1);
-                result.Data.Single().Request.Should().NotBeNull();
-                result.Data.Single().Request.SubscriberName.Should().Be("test-sub");
-            }
+            // Assert
+            var expected = new OperationResult<IEnumerable<PutSubscriberFile>>(
+                new[]
+                {
+                    new PutSubscriberFile
+                    {
+                        Request = new PutSubscriberRequest
+                        {
+                            SubscriberName = "test-sub",
+                            EventName = "test-event",
+                        }
+                    }
+                });
+
+            result.Should().BeEquivalentTo(expected, opt => opt
+                .Excluding(info => info.SelectedMemberInfo.MemberType == typeof(CaptainHookContractSubscriberDto))
+                .Excluding(info => info.SelectedMemberInfo.MemberType == typeof(FileInfoBase)));
+
         }
 
-        public static Dictionary<string, MockFileData> GetMockFiles()
+        [Theory, IsUnit]
+        [InlineData(MockCurrentDirectory)]
+        public void ProcessDirectory_WithMultipleValidFiles_ReturnsValidObject(string testFilesDirectoryPath)
+        {
+            var subscribersDirectoryProcessor = new SubscribersDirectoryProcessor(new MockFileSystem(GetMockFiles2(), MockCurrentDirectory));
+            // Act
+            var result = subscribersDirectoryProcessor.ProcessDirectory(testFilesDirectoryPath);
+
+            // Assert
+            var expected = new OperationResult<IEnumerable<PutSubscriberFile>>(
+                new[]
+                {
+                    new PutSubscriberFile
+                    {
+                        Request = new PutSubscriberRequest
+                        {
+                            SubscriberName = "test-sub",
+                            EventName = "test-event",
+                        }
+                    },
+                    new PutSubscriberFile
+                    {
+                        Request = new PutSubscriberRequest
+                        {
+                            SubscriberName = "test-sub2",
+                            EventName = "test-event2",
+                        }
+                    }
+                });
+
+            result.Should().BeEquivalentTo(expected, opt => opt
+                .Excluding(info => info.SelectedMemberInfo.MemberType == typeof(CaptainHookContractSubscriberDto))
+                .Excluding(info => info.SelectedMemberInfo.MemberType == typeof(FileInfoBase)));
+
+        }
+        
+        private static Dictionary<string, MockFileData> GetMockFiles1()
         {
             Dictionary<string, MockFileData> mockFiles = new Dictionary<string, MockFileData>();
-            mockFiles.Add("TestFiles/sample1.json", new MockFileData(@"
+            mockFiles.Add("sample1.json", new MockFileData(@"
 {
   ""subscriberName"": ""test-sub"",
   ""eventName"": ""test-event"",
@@ -84,5 +134,58 @@ namespace Platform.Eda.Cli.Tests.Commands.ConfigureEda
             return mockFiles;
         }
 
+        private static Dictionary<string, MockFileData> GetMockFiles2()
+        {
+            var mockFiles = new Dictionary<string, MockFileData>
+            {
+                {
+                    "sample1.json", new MockFileData(@"
+{
+  ""subscriberName"": ""test-sub"",
+  ""eventName"": ""test-event"",
+  ""subscriber"": {
+    ""webhooks"": {
+      ""endpoints"": [
+        {
+          ""uri"": ""https://blah.blah/testing"",
+          ""authentication"": {
+            ""type"": ""Basic"",
+            ""username"": ""test"",
+            ""passwordKeyName"": ""AzureSubscriptionId""
+          },
+          ""httpVerb"": ""post""
+        }
+      ]
+    }
+  }
+}
+")
+                },
+                {
+                    "subdir/sample2.json", new MockFileData(@"
+{
+  ""subscriberName"": ""test-sub2"",
+  ""eventName"": ""test-event2"",
+  ""subscriber"": {
+    ""webhooks"": {
+      ""endpoints"": [
+        {
+          ""uri"": ""https://blah.blah/testing2"",
+          ""authentication"": {
+            ""type"": ""Basic"",
+            ""username"": ""test2"",
+            ""passwordKeyName"": ""AzureSubscriptionId""
+          },
+          ""httpVerb"": ""post""
+        }
+      ]
+    }
+  }
+}
+")
+                }
+            };
+            return mockFiles;
+        }
     }
 }
