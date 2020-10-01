ab0e0deb1b6f049f179ff3e85bfe9e4d1e526819
dawidkeshop <dkaliszewski@eshopworld.com>
2020-10-01 15:15:50 +0200

    CLI accepts params replacements

---
+++ src/Tests/Platform.Eda.Cli.Tests/Commands/ConfigureEda/OptionsValidation/ReplacementParamsDictionaryAttributeTests.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Eshopworld.Tests.Core;
+using FluentAssertions;
+using Platform.Eda.Cli.Commands.ConfigureEda.OptionsValidation;
+using Xunit;
+
+namespace Platform.Eda.Cli.Tests.Commands.ConfigureEda.OptionsValidation
+{
+    public class ReplacementParamsDictionaryAttributeTests
+    {
+        [Fact, IsUnit]
+        public void ForValidParam_ReturnsTrue()
+        {
+            var result = new ReplacementParamsDictionaryAttribute().GetValidationResult("abc=def", new ValidationContext(new object()));
+
+            result.Should().Be(ValidationResult.Success);
+        }
+
+        [Theory, IsUnit]
+        [InlineData("abc=")]
+        [InlineData("=def")]
+        [InlineData("abc")]
+        public void ForInvalidParam_ReturnsError(string invalidValue)
+        {
+            var result = new ReplacementParamsDictionaryAttribute().GetValidationResult(invalidValue, new ValidationContext(new object()));
+
+            result.Should().NotBe(ValidationResult.Success);
+        }
+    }
+}
