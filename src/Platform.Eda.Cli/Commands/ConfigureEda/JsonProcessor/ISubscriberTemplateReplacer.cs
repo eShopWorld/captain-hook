882832ea230031b0da867f17c0dd769695133c4e
Nikhil Khandelwal <nkhandelwal@eshopworld.com>
2020-10-02 11:35:05 +0100

    Made interface public.

--- src/Platform.Eda.Cli/Commands/ConfigureEda/JsonProcessor/ISubscriberTemplateReplacer.cs
+++ src/Platform.Eda.Cli/Commands/ConfigureEda/JsonProcessor/ISubscriberTemplateReplacer.cs
@@ -3,7 +3,7 @@ using Newtonsoft.Json.Linq;
 
 namespace Platform.Eda.Cli.Commands.ConfigureEda.JsonProcessor
 {
-    interface ISubscriberTemplateReplacer
+    public interface ISubscriberTemplateReplacer
     {
         public JObject Replace(JObject fileContent, Dictionary<string, JToken> variables);
     }
