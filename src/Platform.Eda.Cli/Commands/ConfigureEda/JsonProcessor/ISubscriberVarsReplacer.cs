87bcd91c8f0839f11c41ce47a9125b05d2d955a7
Nikhil Khandelwal <nkhandelwal@eshopworld.com>
2020-10-02 01:02:58 +0100

    Initial changes.

---
+++ src/Platform.Eda.Cli/Commands/ConfigureEda/JsonProcessor/ISubscriberVarsReplacer.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+
+namespace Platform.Eda.Cli.Commands.ConfigureEda.JsonProcessor
+{
+    interface ISubscriberVarsReplacer
+    {
+        public JObject ReplaceVars(JObject fileContent, Dictionary<string, Dictionary<string, string>> variables);
+    }
+}
---
+++ src/Platform.Eda.Cli/Commands/ConfigureEda/JsonProcessor/SubscriberVarsReplacer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json.Linq;
+
+namespace Platform.Eda.Cli.Commands.ConfigureEda.JsonProcessor
+{
+    public class SubscriberVarsReplacer : ISubscriberVarsReplacer
+    {
+        public JObject ReplaceVars(JObject fileContent, Dictionary<string, Dictionary<string, string>> variables)
+        {
+            foreach (var (propertyKey, envDictionary) in variables)
+            {
+                foreach (var (envKey, value) in envDictionary)
+                {
+                    string variableName = $"vars:{propertyKey}.{envKey}";
+                    var references = fileContent.SelectTokens($"$..[*{variableName}]");
+                }
+
+            }
+            throw new NotImplementedException();
+        }
+    }
+}
