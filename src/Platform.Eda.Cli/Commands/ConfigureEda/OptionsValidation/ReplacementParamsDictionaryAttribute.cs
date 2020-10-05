a389faca990605d71b8a1e466833bf03c71c9317
dawidkeshop <63288056+dawidkeshop@users.noreply.github.com>
2020-10-05 12:53:46 +0200

    CLI accepts params replacements (#313)

---
+++ src/Platform.Eda.Cli/Commands/ConfigureEda/OptionsValidation/ReplacementParamsDictionaryAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace Platform.Eda.Cli.Commands.ConfigureEda.OptionsValidation
+{
+    public class ReplacementParamsDictionaryAttribute : ValidationAttribute
+    {
+        private static readonly Regex KeyValueRegex = new Regex("^\\S+=\\S+$", RegexOptions.Compiled);
+
+        public ReplacementParamsDictionaryAttribute()
+            : base("The value for {0} must be a key and value separated by '='")
+        {
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext context)
+        {
+            if (value == null || (value is string str && !KeyValueRegex.IsMatch(str)))
+            {
+                return new ValidationResult(FormatErrorMessage(context.DisplayName));
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
