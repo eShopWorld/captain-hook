e33ceef1d29179c00d419d7a70eba83a08c02748
Nikhil Khandelwal <nkhandelwal@eshopworld.com>
2020-10-05 16:51:15 +0100

    Added tests

--- src/Tests/Platform.Eda.Cli.Tests/Commands/ConfigureEda/Processor/SubscriberVarsReplacerTests.cs
+++ src/Tests/Platform.Eda.Cli.Tests/Commands/ConfigureEda/Processor/SubscriberVarsReplacerTests.cs
@@ -4,6 +4,7 @@ using System.Text;
 using CaptainHook.Api.Client.Models;
 using Eshopworld.Tests.Core;
 using FluentAssertions;
+using FluentAssertions.Execution;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Platform.Eda.Cli.Commands.ConfigureEda.JsonProcessor;
@@ -14,8 +15,224 @@ namespace Platform.Eda.Cli.Tests.Commands.ConfigureEda.Processor
 {
     public class SubscriberVarsReplacerTests
     {
+        private SubscriberTemplateReplacer _subscriberVarsReplacer;
+
+        public SubscriberVarsReplacerTests()
+        {
+            _subscriberVarsReplacer = new SubscriberTemplateReplacer();
+        }
+
+        [Theory, IsUnit]
+        [InlineData("https://ci-sample.url/api/retailer")]
+        [InlineData("https://test-sample.url/api/retailer")]
+        public void Replace_StringValues_ReplacesCorrectValue(string varValue)
+        {
+            // Arrange
+            var input = @"{
+    ""eventName"": ""sample.event"",
+    ""subscriberName"": ""invoicing"",
+    ""subscriber"": {  
+        ""webhooks"": {      
+            ""endpoints"": [
+                {
+                    ""uri"": ""{vars:retailer-url}"",
+                    ""httpVerb"": ""POST"",
+                    ""selector"": ""*""
+                }
+            ]
+        }
+    }
+}";
+
+            var vars = new Dictionary<string, JToken>
+            {
+                {"retailer-url", JToken.Parse($@"""{varValue}""")}
+            };
+
+            // Act
+            var returnJObject = _subscriberVarsReplacer.Replace(TemplateReplacementType.Vars, input, vars);
+
+            // Assert
+            returnJObject.Data.Should().Be($@"{{
+    ""eventName"": ""sample.event"",
+    ""subscriberName"": ""invoicing"",
+    ""subscriber"": {{  
+        ""webhooks"": {{      
+            ""endpoints"": [
+                {{
+                    ""uri"": ""{varValue}"",
+                    ""httpVerb"": ""POST"",
+                    ""selector"": ""*""
+                }}
+            ]
+        }}
+    }}
+}}");
+        }
+
         [Fact, IsUnit]
-        public void Replace_Vars_ReturnsValidString()
+        public void Replace_WhenReplacementIsString_ReplacedWithString()
+        {
+            // Arrange
+            var replacements = new Dictionary<string, JToken>
+            {
+                {"evo-host", (JToken) "sandbox.eshopworld.com"}
+            };
+
+            const string source = @"
+                    {
+                      ""eventName"": ""eshopworld.platform.events.oms.ordercancelfailedevent"",
+                      ""subscriberName"": ""retailers"",
+                      ""webhooks"": {
+                        ""selectionRule"": ""$.TenantCode"",
+                        ""payloadTransform"": ""Response"",
+                        ""endpoints"": [
+                          {
+                            ""uri"": ""https://order-transaction-api-{selector}.{params:evo-host}/api/v2.0/WebHook/ClientOrderFailureMethod"",
+                            ""selector"": ""*"",
+                            ""httpVerb"": ""POST""
+                          },
+                        ]
+                      },
+                      ""callbacks"": {
+                        ""selectionRule"": ""$.TenantCode"",
+                        ""endpoints"": [
+                          {
+                            ""uri"": ""https://order-transaction-api-{selector}.{params:evo-host}/api/v2/EdaResponse/ExternalEdaResponse"",
+                            ""selector"": ""*"",
+                            ""httpVerb"": ""POST""
+                          }
+                        ]
+                      },
+                      ""dlqhooks"": {
+                        ""selectionRule"": ""$.TenantCode"",
+                        ""endpoints"": [
+                          {
+                            ""uri"": ""https://order-transaction-api-{selector}.{params:evo-host}/api/v2/DlqRequest/ExternalRequest"",
+                            ""selector"": ""*"",
+                            ""httpVerb"": ""POST""
+                          }
+                        ]
+                      }
+                    }
+            ";
+
+            // Act
+            var result = _subscriberVarsReplacer.Replace(TemplateReplacementType.Params, source, replacements);
+
+            // Assert
+            using (new AssertionScope())
+            {
+                result.IsError.Should().BeFalse();
+                result.Data.Should().Be(@"
+                    {
+                      ""eventName"": ""eshopworld.platform.events.oms.ordercancelfailedevent"",
+                      ""subscriberName"": ""retailers"",
+                      ""webhooks"": {
+                        ""selectionRule"": ""$.TenantCode"",
+                        ""payloadTransform"": ""Response"",
+                        ""endpoints"": [
+                          {
+                            ""uri"": ""https://order-transaction-api-{selector}.sandbox.eshopworld.com/api/v2.0/WebHook/ClientOrderFailureMethod"",
+                            ""selector"": ""*"",
+                            ""httpVerb"": ""POST""
+                          },
+                        ]
+                      },
+                      ""callbacks"": {
+                        ""selectionRule"": ""$.TenantCode"",
+                        ""endpoints"": [
+                          {
+                            ""uri"": ""https://order-transaction-api-{selector}.sandbox.eshopworld.com/api/v2/EdaResponse/ExternalEdaResponse"",
+                            ""selector"": ""*"",
+                            ""httpVerb"": ""POST""
+                          }
+                        ]
+                      },
+                      ""dlqhooks"": {
+                        ""selectionRule"": ""$.TenantCode"",
+                        ""endpoints"": [
+                          {
+                            ""uri"": ""https://order-transaction-api-{selector}.sandbox.eshopworld.com/api/v2/DlqRequest/ExternalRequest"",
+                            ""selector"": ""*"",
+                            ""httpVerb"": ""POST""
+                          }
+                        ]
+                      }
+                    }
+            ");
+            }
+        }
+
+        [Fact, IsUnit]
+        public void Replace_ReplacementIsObject_ReplacedWithObject()
+        {
+            // Arrange
+            var replacements = new Dictionary<string, JToken>
+            {
+                {"sts-settings", JToken.Parse("{ \"type\": \"OIDC\" } ")},
+            };
+
+            var source = @"
+                    {
+                      ""eventName"": ""eshopworld.platform.events.oms.ordercancelfailedevent"",
+                      ""subscriberName"": ""retailers"",
+                      ""webhooks"": {
+                        ""selectionRule"": ""$.TenantCode"",
+                        ""payloadTransform"": ""Response"",
+                        ""endpoints"": [
+                          {
+                            ""uri"": ""https://order-transaction-api-{selector}.{params:evo-host}/api/v2.0/WebHook/ClientOrderFailureMethod"",
+                            ""selector"": ""*"",
+                            ""authentication"": ""{vars:sts-settings}"",
+                            ""httpVerb"": ""POST""
+                          },
+                        ]
+                      },
+                      ""callbacks"": {
+                        ""selectionRule"": ""$.TenantCode"",
+                        ""endpoints"": [
+                          {
+                            ""uri"": ""https://order-transaction-api-{selector}.{params:evo-host}/api/v2/EdaResponse/ExternalEdaResponse"",
+                            ""selector"": ""*"",
+                            ""authentication"": ""{vars:sts-settings}"",
+                            ""httpVerb"": ""POST""
+                          }
+                        ]
+                      },
+                      ""dlqhooks"": {
+                        ""selectionRule"": ""$.TenantCode"",
+                        ""endpoints"": [
+                          {
+                            ""uri"": ""https://order-transaction-api-{selector}.{params:evo-host}/api/v2/DlqRequest/ExternalRequest"",
+                            ""selector"": ""*"",
+                            ""authentication"": ""{vars:sts-settings}"",
+                            ""httpVerb"": ""POST""
+                          }
+                        ]
+                      }
+                    }
+            ";
+
+            // Act
+            var result = _subscriberVarsReplacer.Replace(TemplateReplacementType.Vars, source, replacements);
+
+            // Assert
+            result.IsError.Should().BeFalse();
+            var resultJObject = JObject.Parse(result.Data);
+            resultJObject.Should().NotBeNull();
+
+            using (new AssertionScope())
+            {
+                resultJObject["webhooks"]["endpoints"][0]["authentication"]["type"].Value<string>().Should().Be("OIDC");
+                resultJObject["callbacks"]["endpoints"][0]["authentication"]["type"].Value<string>().Should()
+                    .Be("OIDC");
+                resultJObject["dlqhooks"]["endpoints"][0]["authentication"]["type"].Value<string>().Should().Be("OIDC");
+            }
+        }
+
+        [Fact, IsUnit]
+        public void Replace_VarsContainingParams_ReturnsValidString()
         {
             var varsDictionary = new Dictionary<string, JToken>
             {
@@ -29,7 +246,7 @@ namespace Platform.Eda.Cli.Tests.Commands.ConfigureEda.Processor
             ""type"": ""OIDC""}"),
             };
 
-            var result = new SubscriberTemplateReplacer().Replace(TemplateReplacementType.Vars, JObjectWithVars, varsDictionary);
+            var result = _subscriberVarsReplacer.Replace(TemplateReplacementType.Vars, JObjectWithVars, varsDictionary);
 
             var expected = new PutSubscriberRequest
             {
@@ -64,7 +281,7 @@ namespace Platform.Eda.Cli.Tests.Commands.ConfigureEda.Processor
             };
 
             var serializeObject = JsonConvert.SerializeObject(expected, Formatting.Indented, new JsonSerializerSettings() { NullValueHandling = NullValueHandling.Ignore });
-
+            
             result.Data.ToLowerInvariant().Replace(" ", "").Should().BeEquivalentTo(serializeObject.ToLowerInvariant().Replace(" ", ""));
         }
 
@@ -87,39 +304,5 @@ namespace Platform.Eda.Cli.Tests.Commands.ConfigureEda.Processor
 }";
 
 
-        //        [Theory, IsUnit]
-        //        [InlineData("https://ci-sample.url/api/doSomething")]
-        //        [InlineData("https://test-sample.url/api/doSomething")]
-        //        public void ReplaceVars_StringValues_ReplacesCorrectValue(string varValue)
-        //        {
-        //            // Arrange
-        //            var input = JObject.Parse(@"{
-        //    ""eventName"": ""sample.event"",
-        //    ""subscriberName"": ""invoicing"",
-        //    ""subscriber"": {  
-        //        ""webhooks"": {      
-        //            ""endpoints"": [
-        //                {
-        //                    ""uri"": ""{vars:retailer-url}"",
-        //                    ""httpVerb"": ""POST"",
-        //                    ""selector"": ""*""
-        //                }
-        //            ]
-        //        }
-        //    }
-        //}");
-
-        //            var subscriberVarsReplacer = new SubscriberTemplateReplacer(TemplateReplacementType.Vars);
-        //            var vars = new Dictionary<string, JToken>
-        //            {
-        //                {"retailer-url", JToken.Parse($@"""{varValue}""")}
-        //            };
-
-        //            // Act
-        //            var returnJObject = subscriberVarsReplacer.Replace(input, vars);
-
-        //            // Assert
-        //            returnJObject.SelectToken("$.subscriber.webhooks.endpoints[0].uri")!.ToString().Should().Be(varValue);
-        //        }
     }
 }
