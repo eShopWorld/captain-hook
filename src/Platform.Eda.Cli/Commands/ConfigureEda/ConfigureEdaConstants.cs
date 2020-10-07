503117b76135ebbc4059a882c585c15e45d3e425
Nikhil Khandelwal <nkhandelwal@eshopworld.com>
2020-10-07 15:36:09 +0100

    Formatted error messages.

--- src/Platform.Eda.Cli/Commands/ConfigureEda/ConfigureEdaConstants.cs
+++ src/Platform.Eda.Cli/Commands/ConfigureEda/ConfigureEdaConstants.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using NotImplementedException = System.NotImplementedException;
+﻿using System.Linq;
 
 namespace Platform.Eda.Cli.Commands.ConfigureEda
 {
--- src/Platform.Eda.Cli/Commands/ConfigureEda/JsonProcessor/PutSubscriberProcessChain.cs
+++ src/Platform.Eda.Cli/Commands/ConfigureEda/JsonProcessor/PutSubscriberProcessChain.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.IO.Abstractions;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
