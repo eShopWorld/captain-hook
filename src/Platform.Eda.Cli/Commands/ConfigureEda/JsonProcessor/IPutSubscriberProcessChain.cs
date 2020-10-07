4f0bf3fcab5be8c0cfa29b5d9b36629e77d1c86a
Antonino Parisi <aparisi@eshopworld.com>
2020-10-07 10:48:43 +0100

    Add interface description

--- src/Platform.Eda.Cli/Commands/ConfigureEda/JsonProcessor/IPutSubscriberProcessChain.cs
+++ src/Platform.Eda.Cli/Commands/ConfigureEda/JsonProcessor/IPutSubscriberProcessChain.cs
@@ -5,6 +5,14 @@ namespace Platform.Eda.Cli.Commands.ConfigureEda.JsonProcessor
 {
     public interface IPutSubscriberProcessChain
     {
+        /// <summary>
+        /// Executes the main process pipeline
+        /// </summary>
+        /// <param name="inputFolderPath">The input folder</param>
+        /// <param name="env">The environment name</param>
+        /// <param name="replacementParams">The replacement parameters</param>
+        /// <param name="noDryRun">Whether to perform a dry run or not</param>
+        /// <returns>Error code</returns>
         Task<int> Process(string inputFolderPath, string env, Dictionary<string, string> replacementParams, bool noDryRun);
     }
 }
