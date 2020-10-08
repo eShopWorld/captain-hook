3ea61dea9b1889821ac0cd2f58f8111a2dfc40b4
dawidkeshop <dkaliszewski@eshopworld.com>
2020-10-08 18:06:55 +0200

    Added validation attribute to verify if directory exists

---
+++ src/Platform.Eda.Cli/Commands/ConfigureEda/OptionsValidation/DirectoryExistsValidationAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.IO;
+
+namespace Platform.Eda.Cli.Commands.ConfigureEda.OptionsValidation
+{
+    public class DirectoryExistsValidationAttribute : ValidationAttribute
+    {
+        public DirectoryExistsValidationAttribute()
+            : base("Directory '{0}' doesn't exist.")
+        {
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext context)
+        {
+            if (value is string s && Directory.Exists(s))
+                return ValidationResult.Success;
+
+            return new ValidationResult(string.Format(ErrorMessageString, value));
+        }
+    }
+}
