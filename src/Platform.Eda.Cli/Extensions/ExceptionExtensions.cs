5171f2ad49b975b47dbb7324bc144c6396e998f4
Antonino Parisi <aparisi@eshopworld.com>
2020-10-13 14:08:00 +0100

    #89940 Improve CLI error logging

---
+++ src/Platform.Eda.Cli/Extensions/ExceptionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Platform.Eda.Cli.Extensions
+{
+    [ExcludeFromCodeCoverage]
+    public static class ExceptionExtensions
+    {
+        public static IEnumerable<Exception> InnerExceptions(this Exception exception)
+        {
+            var currentException = exception;
+
+            while (currentException != null)
+            {
+                yield return currentException;
+                currentException = currentException.InnerException;
+            }
+        }
+    }
+}
