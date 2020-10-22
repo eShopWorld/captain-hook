bfd08ed4437be680d0bf12412760619c3a5b02cc
Antonino Parisi <tabman83@gmail.com>
2020-10-22 15:34:07 +0100

    #91447 Case insensitive deserialization for replacement dictionary (#364)
    * Fix subscribers merge issue
    
    * Case insensitive deserialization for replacement dictionary
    
    * Add unit tests
    
    * Use case insensitive dictionary at entity level
    
    * Addressed comments
    
    * Use IReadOnlyDictionary interface for UriTransformEntity replacements
    
    * Revert last change
    
    * Check for case sensitivity in webhooksentityvalidatortests
    
    * Add one more case to Validate_WebhookWithAllTokensInUriTransform_SucceedsValidation

--- src/CaptainHook.Domain/Entities/UriTransformEntity.cs
+++ src/CaptainHook.Domain/Entities/UriTransformEntity.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 
 namespace CaptainHook.Domain.Entities
 {
     public class UriTransformEntity
     {
+        private static readonly Dictionary<string, string> EmptyReplacementsDictionary = new Dictionary<string, string>();
+
         /// <summary>
         /// A list of replacements
         /// </summary>
@@ -12,7 +15,10 @@ namespace CaptainHook.Domain.Entities
 
         public UriTransformEntity(IDictionary<string, string> replace)
         {
-            Replace = new ReadOnlyDictionary<string, string>(replace ?? new Dictionary<string, string>());
+            var caseInsensitiveDictionary = new Dictionary<string, string>(
+                replace ?? EmptyReplacementsDictionary,
+                StringComparer.OrdinalIgnoreCase);
+            Replace = new ReadOnlyDictionary<string, string>(caseInsensitiveDictionary);
         }
     }
 }
--- src/CaptainHook.Storage.Cosmos/Models/UriTransformSubdocument.cs
+++ src/CaptainHook.Storage.Cosmos/Models/UriTransformSubdocument.cs
@@ -5,11 +5,13 @@ namespace CaptainHook.Storage.Cosmos.Models
 {
     public class UriTransformSubdocument
     {
+        private static readonly Dictionary<string, string> EmptyReplacementsDictionary = new Dictionary<string, string>();
+
         public UriTransformSubdocument(IDictionary<string, string> replace)
         {
-            Replace = new ReadOnlyDictionary<string, string>(replace ?? new Dictionary<string, string>());
+            Replace = new ReadOnlyDictionary<string, string>(replace ?? EmptyReplacementsDictionary);
         }
-
+        
         public IDictionary<string, string> Replace { get; }
     }
 }
