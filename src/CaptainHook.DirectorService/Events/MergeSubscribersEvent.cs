6383d9dc9c071fc9225e8d2a58c0465151032516
dawidkeshop <dkaliszewski@eshopworld.com>
2020-11-10 12:16:46 +0100

    MergeSubscribersEvent created and logged

---
+++ src/CaptainHook.DirectorService/Events/MergeSubscribersEvent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Eshopworld.Core;
+using Newtonsoft.Json;
+
+namespace CaptainHook.DirectorService.Events
+{
+    public class MergeSubscribersEvent : TelemetryEvent
+    {
+        public string OnlyInKeyVault { get; set; }
+
+        public MergeSubscribersEvent(IEnumerable<string> onlyInKeyVault)
+        {
+            OnlyInKeyVault = JsonConvert.SerializeObject(onlyInKeyVault);
+        }
+    }
+}
