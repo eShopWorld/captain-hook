32952395ff05948ec7b1048c3093579bd49d662d
dawidkeshop <dkaliszewski@eshopworld.com>
2021-01-15 15:50:36 +0100

    Code cleanup

--- src/CaptainHook.EventReaderService/Program.cs
+++ src/CaptainHook.EventReaderService/Program.cs
@@ -30,31 +30,18 @@ namespace CaptainHook.EventReaderService
                     {
                         {"cm--ai-telemetry--instrumentation", "Telemetry:InstrumentationKey"},
                         {"cm--ai-telemetry--internal", "Telemetry:InternalKey"},
-                        //{"cm--cosmos-connection--esw-platform", "CosmosDB:ConnectionString"},
                         {"cm--sb-connection--esw-eda", $"{nameof(ServiceBusSettings)}:{nameof(ServiceBusSettings.ConnectionString)}"},
-                        //{"cm--sb-subscription-id--esw-eda", $"{nameof(ServiceBusSettings)}:{nameof(ServiceBusSettings.SubscriptionId)}"},
                     }).Build();
 
-                //var configuration = TempConfigLoader.Load();
-                //var configurationSettings = configuration.Get<ConfigurationSettings>();
 
                 var telemetrySettings = configuration.GetSection("Telemetry").Get<TelemetrySettings>();
                 var serviceBusSettings = configuration.GetSection(nameof(ServiceBusSettings)).Get<ServiceBusSettings>();
 
                 var builder = new ContainerBuilder();
-                //builder.RegisterInstance(configurationSettings).SingleInstance();
                 builder.RegisterInstance(serviceBusSettings).SingleInstance();
                 builder.RegisterType<MessageProviderFactory>().As<IMessageProviderFactory>().SingleInstance();
                 builder.RegisterType<ServiceBusManager>().As<IServiceBusManager>();
                 builder.RegisterType<MessageLockDurationCalculator>().As<IMessageLockDurationCalculator>().SingleInstance();
-
-                //// temporary fix for ServiceBusManager, will be improved in the next task
-                //var serviceBusSettings = new ServiceBusSettings
-                //{
-                //    ConnectionString = configurationSettings.ServiceBusConnectionString,
-                //    SubscriptionId = configurationSettings.AzureSubscriptionId
-                //};
-                //builder.RegisterInstance(serviceBusSettings).SingleInstance();
 
                 //SF Deps
                 builder.Register<IActorProxyFactory>(_ => new ActorProxyFactory());
