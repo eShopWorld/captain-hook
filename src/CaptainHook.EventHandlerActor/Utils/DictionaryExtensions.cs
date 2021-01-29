268a9bf5b41b337bfeaeb4c6a72f7a799d853687
dawidkeshop <63288056+dawidkeshop@users.noreply.github.com>
2021-01-29 14:38:29 +0100

    107415 RequestLogger fixes (#404)
    * RequestHeaders logging improved
    
    * Fixed missing Authorization header issue

---
+++ src/CaptainHook.EventHandlerActor/Utils/DictionaryExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CaptainHook.EventHandlerActor.Utils
+{
+    public static class DictionaryExtensions
+    {
+        public static string ToDebugString(this Dictionary<string, string> dictionary)
+        {
+            if (dictionary == null)
+                return "null";
+
+            if (dictionary.Count == 0)
+                return "empty";
+
+            var stringBuilder = new StringBuilder();
+            foreach (var (key, value) in dictionary)
+                stringBuilder.Append($"{key}: {value}{Environment.NewLine}");
+            return stringBuilder.ToString();
+        }
+    }
+}
